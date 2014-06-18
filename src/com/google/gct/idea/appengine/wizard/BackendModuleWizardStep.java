e1f0bc1e27a7a576ee96e83616beff1fa871630e
Dmitry Jemerov <yole@google.com>
2014-06-18 12:45:48 +0200

    Add missing listener for updating client module name.
    Change-Id: I879b51ec8a5b890b04bc6217a0f5db4a4c5bafdc

--- src/com/google/gct/idea/appengine/wizard/BackendModuleWizardStep.java
+++ src/com/google/gct/idea/appengine/wizard/BackendModuleWizardStep.java
@@ -34,6 +34,8 @@ import org.jetbrains.android.facet.AndroidFacet;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -70,6 +72,12 @@ public class BackendModuleWizardStep extends ModuleWizardStep implements Android
     mySidePanelIcon = sidePanelIcon;
     setupListener(myModuleNameField, true);
     setupListener(myPackageNameField, false);
+    myClientModuleCombo.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent actionEvent) {
+        updateDataModel();
+      }
+    });
 
     List<Module> clientModules = new ArrayList<Module>();
     Module[] modules = ModuleManager.getInstance(project).getModules();
