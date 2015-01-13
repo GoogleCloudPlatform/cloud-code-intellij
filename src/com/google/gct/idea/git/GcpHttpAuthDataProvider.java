ee4e9a92fccf7ce9d3988ea224d2e9affad5e2ae
Benjamin Wulfe <benwu@google.com>
2015-01-13 17:22:54 -0500

    remove obsolete class from gcp checkoutprovider
    Change-Id: I2d780896bb4534de2099ca3933d5fb2736705493

--- src/com/google/gct/idea/git/GcpHttpAuthDataProvider.java
+++ src/com/google/gct/idea/git/GcpHttpAuthDataProvider.java
@@ -37,7 +37,7 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.wm.ex.WindowManagerEx;
 import com.intellij.util.AuthData;
 import git4idea.DialogManager;
-import git4idea.jgit.GitHttpAuthDataProvider;
+import git4idea.remote.GitHttpAuthDataProvider;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
