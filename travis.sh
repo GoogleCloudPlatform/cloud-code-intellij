ff1b8d6adf7cc55fbf980117c56d9f7db2d846b2
Elliotte Rusty Harold <elharo@macfaq.com>
2015-09-04 11:40:00 -0400

    Tell travis how to test this project

---
+++ travis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+./fetchIdea.sh
+
+# Run the tests
+gradle test
+
+# Was our build successful?
+stat=$?
+
+if [ "${TRAVIS}" != true ]; then
+    gradle clean
+    rm -rf idea-IC
+fi
+
+# Return the build status
+exit ${stat}
+
