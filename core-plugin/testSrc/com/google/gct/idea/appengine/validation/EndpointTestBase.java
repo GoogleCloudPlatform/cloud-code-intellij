994a11427e1aeb9d6a78f88c5c1d932adb317988
Elliotte Rusty Harold <elharo@macfaq.com>
2015-09-18 15:21:58 -0400

    fix inspection tests

--- core-plugin/testSrc/com/google/gct/idea/appengine/validation/EndpointTestBase.java
+++ core-plugin/testSrc/com/google/gct/idea/appengine/validation/EndpointTestBase.java
@@ -16,7 +16,15 @@
 
 package com.google.gct.idea.appengine.validation;
 
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.libraries.Library;
+import com.intellij.openapi.roots.libraries.LibraryTable;
+import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar;
 import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase;
 
 import java.io.File;
@@ -27,5 +35,44 @@ public abstract class EndpointTestBase extends JavaCodeInsightFixtureTestCase {
     String homePath = new File(".").getAbsolutePath();
     String homePathParent = homePath.substring(0, homePath.lastIndexOf('/'));
     return homePathParent + FileUtil.toSystemDependentName("/testData/");
+  }
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    addEndpointSdkToProject();
+  }
+
+  /**
+   * Adds the App Engine - Endpoint SDK to the test project's library
+   */
+  private void addEndpointSdkToProject() {
+    LocalFileSystem fs = LocalFileSystem.getInstance();
+    final VirtualFile libDir = fs.findFileByPath(getTestDataPath());
+
+    if (libDir != null) {
+      final VirtualFile pluginsDir = libDir.findChild("lib");
+      if (pluginsDir != null) {
+        ApplicationManager.getApplication().runWriteAction(new Runnable() {
+          @Override
+          public void run() {
+            final LibraryTable table = LibraryTablesRegistrar.getInstance()
+                .getLibraryTableByLevel(LibraryTablesRegistrar.APPLICATION_LEVEL, myModule.getProject());
+            assert table != null;
+            final LibraryTable.ModifiableModel tableModel = table.getModifiableModel();
+            final Library library = tableModel.createLibrary("lib");
+            final Library.ModifiableModel libraryModel = library.getModifiableModel();
+            libraryModel.addJarDirectory(pluginsDir, true);
+            libraryModel.commit();
+            tableModel.commit();
+
+            ModifiableRootModel rootModel = ModuleRootManager.getInstance(myModule).getModifiableModel();
+            Library jar = table.getLibraries()[0];
+            rootModel.addLibraryEntry(jar); // Endpoint is the only jar added
+            rootModel.commit();
+          }
+        });
+      }
+    }
   }
 }
