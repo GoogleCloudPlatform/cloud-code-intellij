cc622d476409682c8435a21b4ac0b09a39bfe3d3
Etan Shaul <eshaul@google.com>
2016-02-18 14:34:53 -0500

    using service manager to retrieve container service

--- google-cloud-tools-plugin/src/com/google/gct/login/IntelliJGoogleLoginMessageExtender.java
+++ google-cloud-tools-plugin/src/com/google/gct/login/IntelliJGoogleLoginMessageExtender.java
@@ -3,20 +3,19 @@ package com.google.gct.login;
 import com.google.gct.idea.debugger.CloudDebugProcessStateCollector;
 import com.google.gct.idea.util.GctBundle;
 
-import com.intellij.openapi.application.ApplicationManager;
-
 public class IntelliJGoogleLoginMessageExtender implements GoogleLoginMessageExtender {
 
   private final CloudDebugProcessStateCollector stateCollector;
 
   public IntelliJGoogleLoginMessageExtender() {
-    stateCollector = ApplicationManager.getApplication()
-                                       .getComponent(CloudDebugProcessStateCollector.class);
+    stateCollector = CloudDebugProcessStateCollector.getInstance();
   }
 
   @Override
   public String additionalLogoutMessage() {
-    if (!stateCollector.getBackgroundListeningStates().isEmpty()) {
+     if (stateCollector != null
+        && stateCollector.getBackgroundListeningStates() != null
+        && !stateCollector.getBackgroundListeningStates().isEmpty()) {
       return GctBundle.message("clouddebug.logout.additional.message");
     } else {
       return "";
