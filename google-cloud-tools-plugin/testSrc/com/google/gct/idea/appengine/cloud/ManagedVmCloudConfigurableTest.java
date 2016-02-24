90b9a36c64eb922183779c62aeafa1ac3dccd1c0
Etan Shaul <eshaul@google.com>
2016-02-24 11:52:59 -0500

    validating project selection for managed vm deployment

--- google-cloud-tools-plugin/testSrc/com/google/gct/idea/appengine/cloud/ManagedVmCloudConfigurableTest.java
+++ google-cloud-tools-plugin/testSrc/com/google/gct/idea/appengine/cloud/ManagedVmCloudConfigurableTest.java
@@ -20,27 +20,34 @@ import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import com.google.gct.idea.elysium.ProjectSelector;
 import com.google.gct.idea.util.SystemEnvironmentProvider;
 
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import com.intellij.testFramework.PlatformTestCase;
 
+import org.junit.rules.TemporaryFolder;
 import org.picocontainer.MutablePicoContainer;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.swing.JLabel;
 
 public class ManagedVmCloudConfigurableTest extends PlatformTestCase {
+  private ManagedVmCloudConfigurable managedVmCloudConfigurable;
   private SystemEnvironmentProvider environmentProvider;
+  private ProjectSelector projectSelector;
   private JLabel warningMessage;
   private TextFieldWithBrowseButton cloudSdkDirectoryField;
 
   private static final String CLOUD_SDK_EXECUTABLE_PATH = "/a/b/c/gcloud";
   private static final String CLOUD_SDK_DIR_PATH = "/a/b/c";
 
-  private static final String INVALID_SDK_DIR_WARNING = "Please select a Cloud SDK directory.";
+  private static final String MISSING_PROJECT_WARNING = "Please select a project.";
+  private static final String MISSING_SDK_DIR_WARNING = "Please select a Cloud SDK directory.";
 
   @Override
   public void setUp() throws Exception {
@@ -52,7 +59,8 @@ public class ManagedVmCloudConfigurableTest extends PlatformTestCase {
     environmentProvider = mock(SystemEnvironmentProvider.class);
 
     applicationContainer.unregisterComponent(SystemEnvironmentProvider.class.getName());
-    applicationContainer.registerComponentInstance(SystemEnvironmentProvider.class.getName(), environmentProvider);
+    applicationContainer.registerComponentInstance(
+        SystemEnvironmentProvider.class.getName(), environmentProvider);
   }
 
   public void testSetupWithoutSdkInPath() {
@@ -79,7 +87,56 @@ public class ManagedVmCloudConfigurableTest extends PlatformTestCase {
     cloudSdkDirectoryField.setText("/some/invalid/path");
 
     assertTrue(warningMessage.isVisible());
-    assertEquals(INVALID_SDK_DIR_WARNING, warningMessage.getText());
+    assertEquals(MISSING_SDK_DIR_WARNING, warningMessage.getText());
+  }
+
+  public void testApply_validSdkAndValidProject() throws Exception {
+    when(environmentProvider.findInPath(anyString()))
+        .thenReturn(createTempFile());
+    initCloudConfigurable();
+    projectSelector.setText("myProject");
+
+    // No exception should be thrown here
+    managedVmCloudConfigurable.apply();
+  }
+
+  public void testApply_validSdkAndInvalidProject() throws IOException {
+    when(environmentProvider.findInPath(anyString())).thenReturn(createTempFile());
+    initCloudConfigurable();
+    // Do not select project
+
+    try {
+      managedVmCloudConfigurable.apply();
+      fail("Applying settings without a Project should throw exception.");
+    } catch (ConfigurationException ce) {
+      assertEquals(MISSING_PROJECT_WARNING, ce.getMessage());
+    }
+  }
+
+  public void testApply_invalidSdkAndValidProject() {
+    initCloudConfigurable();
+    cloudSdkDirectoryField.setText("/some/invalid/path");
+    projectSelector.setText("myProject");
+
+    try {
+      managedVmCloudConfigurable.apply();
+      fail("Applying settings without a valid SDK should throw exception.");
+    } catch (ConfigurationException ce) {
+      assertEquals(MISSING_SDK_DIR_WARNING, ce.getMessage());
+    }
+  }
+
+  public void testApply_invalidSdkAndInvalidProject() {
+    initCloudConfigurable();
+    cloudSdkDirectoryField.setText("/some/invalid/path");
+    // Do not select project
+
+    try {
+      managedVmCloudConfigurable.apply();
+      fail("Applying settings without a valid SDK and Project should throw exception.");
+    } catch (ConfigurationException ce) {
+      assertEquals(MISSING_SDK_DIR_WARNING, ce.getMessage());
+    }
   }
 
   @Override
@@ -88,9 +145,16 @@ public class ManagedVmCloudConfigurableTest extends PlatformTestCase {
   }
 
   private void initCloudConfigurable() {
-    ManagedVmCloudConfigurable managedVmCloudConfigurable =
+    managedVmCloudConfigurable =
         new ManagedVmCloudConfigurable(new ManagedVmServerConfiguration(), getProject());
+    projectSelector = managedVmCloudConfigurable.getProjectSelector();
     warningMessage = managedVmCloudConfigurable.getWarningMessage();
     cloudSdkDirectoryField = managedVmCloudConfigurable.getCloudSdkDirectoryField();
+  }
+
+  private File createTempFile() throws IOException {
+    TemporaryFolder tempFolder = new TemporaryFolder();
+    tempFolder.create();
+    return tempFolder.newFile("gcloud");
   }
 }
