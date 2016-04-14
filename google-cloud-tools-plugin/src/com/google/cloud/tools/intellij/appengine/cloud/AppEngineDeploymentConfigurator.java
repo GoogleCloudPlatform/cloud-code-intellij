53778528baaaec8809345dec5144e5afa07ea8aa
Patrick Flynn <paflynn@google.com>
2016-04-14 15:55:55 -0400

    Factor out public internal classes of AppEngineCloudType.
    AppEngineCloudType is too big, and contains classes that should be
    scoped at the package level since they are used by other classes in the package.

---
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/cloud/AppEngineDeploymentConfigurator.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.intellij.appengine.cloud;
+
+import com.google.cloud.tools.intellij.appengine.util.CloudSdkUtil;
+
+import com.intellij.openapi.module.ModulePointer;
+import com.intellij.openapi.module.ModulePointerManager;
+import com.intellij.openapi.options.SettingsEditor;
+import com.intellij.openapi.project.Project;
+import com.intellij.packaging.artifacts.Artifact;
+import com.intellij.packaging.artifacts.ArtifactManager;
+import com.intellij.remoteServer.configuration.RemoteServer;
+import com.intellij.remoteServer.configuration.deployment.DeploymentConfigurator;
+import com.intellij.remoteServer.configuration.deployment.DeploymentSource;
+import com.intellij.remoteServer.configuration.deployment.JavaDeploymentSourceUtil;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Sets up the configuration elements for an AppEngine Cloud deployment.
+ */
+class AppEngineDeploymentConfigurator extends
+    DeploymentConfigurator<AppEngineDeploymentConfiguration, AppEngineServerConfiguration> {
+
+  private final Project project;
+
+  public AppEngineDeploymentConfigurator(Project project) {
+    this.project = project;
+  }
+
+  @NotNull
+  @Override
+  public List<DeploymentSource> getAvailableDeploymentSources() {
+    List<DeploymentSource> deploymentSources = new ArrayList<DeploymentSource>();
+
+    ModulePointer modulePointer =
+        ModulePointerManager.getInstance(project).create("userSpecifiedSource");
+    deploymentSources.add(new UserSpecifiedPathDeploymentSource(modulePointer));
+
+    deploymentSources.addAll(JavaDeploymentSourceUtil
+        .getInstance().createArtifactDeploymentSources(project, getJarsAndWars()));
+
+    return deploymentSources;
+  }
+
+  private List<Artifact> getJarsAndWars() {
+    List<Artifact> jarsAndWars = new ArrayList<Artifact>();
+    for (Artifact artifact : ArtifactManager.getInstance(project).getArtifacts()) {
+      if (artifact.getArtifactType().getId().equalsIgnoreCase("jar")
+          || artifact.getArtifactType().getId().equalsIgnoreCase("war")) {
+        jarsAndWars.add(artifact);
+      }
+    }
+
+    Collections.sort(jarsAndWars, ArtifactManager.ARTIFACT_COMPARATOR);
+    return jarsAndWars;
+  }
+
+  @NotNull
+  @Override
+  public AppEngineDeploymentConfiguration createDefaultConfiguration(
+      @NotNull DeploymentSource source) {
+    return new AppEngineDeploymentConfiguration();
+  }
+
+  @Nullable
+  @Override
+  public SettingsEditor<AppEngineDeploymentConfiguration> createEditor(
+      @NotNull DeploymentSource source,
+      @NotNull RemoteServer<AppEngineServerConfiguration> server) {
+    return new AppEngineDeploymentRunConfigurationEditor(project, source,
+        server.getConfiguration(),
+        new CloudSdkAppEngineHelper(
+            new File(
+                CloudSdkUtil.toExecutablePath(server.getConfiguration().getCloudSdkHomePath())),
+            server.getConfiguration().getCloudProjectName(),
+            server.getConfiguration().getGoogleUserName()));
+  }
+}
---
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/cloud/UserSpecifiedPathDeploymentSourceType.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.intellij.appengine.cloud;
+
+import com.google.cloud.tools.intellij.util.GctBundle;
+
+import com.intellij.openapi.module.ModulePointerManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.remoteServer.configuration.deployment.DeploymentSourceType;
+import com.intellij.remoteServer.configuration.deployment.ModuleDeploymentSource;
+import com.intellij.remoteServer.impl.configuration.deployment.DeployToServerRunConfiguration;
+
+import org.jdom.Element;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+/**
+ * A {@link DeploymentSourceType} that supports serialization for a
+ * {@link UserSpecifiedPathDeploymentSource}.
+ */
+public class UserSpecifiedPathDeploymentSourceType extends
+    DeploymentSourceType<ModuleDeploymentSource> {
+
+  private static final String NAME_ATTRIBUTE = "name";
+  private static final String SOURCE_TYPE_ID = "filesystem-war-jar-module";
+
+  public UserSpecifiedPathDeploymentSourceType() {
+    super(SOURCE_TYPE_ID);
+  }
+
+  /**
+   * Restore presentable name (e.g., to be "Filesystem JAR or WAR file - <file path>") of
+   * UserSpecifiedPathDeploymentSource.
+   */
+  @NotNull
+  @Override
+  public ModuleDeploymentSource load(@NotNull Element tag, @NotNull Project project) {
+    UserSpecifiedPathDeploymentSource userSpecifiedSource =
+        new UserSpecifiedPathDeploymentSource(ModulePointerManager
+            .getInstance(project).create(tag.getAttributeValue(NAME_ATTRIBUTE)));
+
+    Element settings = tag.getChild(DeployToServerRunConfiguration.SETTINGS_ELEMENT);
+    if (settings != null) {
+      String filePath = settings.getAttributeValue(
+          AppEngineDeploymentConfiguration.USER_SPECIFIED_ARTIFACT_PATH_ATTRIBUTE);
+
+      if (!StringUtil.isEmpty(filePath)) {
+        userSpecifiedSource.setName(
+            GctBundle.message(
+                "appengine.flex.user.specified.deploymentsource.name.with.filename",
+                new File(filePath).getName()));
+
+        return userSpecifiedSource;
+      }
+    }
+
+    userSpecifiedSource.setName(
+        GctBundle.message("appengine.flex.user.specified.deploymentsource.name"));
+
+    return userSpecifiedSource;
+  }
+
+  @Override
+  public void save(@NotNull ModuleDeploymentSource source, @NotNull Element tag) {
+    tag.setAttribute(NAME_ATTRIBUTE, source.getModulePointer().getModuleName());
+  }
+}
