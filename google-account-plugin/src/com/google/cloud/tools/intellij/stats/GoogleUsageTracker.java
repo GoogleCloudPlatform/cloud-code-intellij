a4ef6009e70cd7b7b76abc970d3a8c6eb639554e
Chanseok Oh <chanseok@google.com>
2016-06-02 15:06:50 -0400

    Remove unused import

--- google-account-plugin/src/com/google/cloud/tools/intellij/stats/GoogleUsageTracker.java
+++ google-account-plugin/src/com/google/cloud/tools/intellij/stats/GoogleUsageTracker.java
@@ -16,7 +16,6 @@
 
 package com.google.cloud.tools.intellij.stats;
 
-import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 
