a19f44698a41bf6e71fbc1ca9501c01d315f5e08
Etan Shaul <eshaul@google.com>
2016-08-04 12:33:28 -0400

    Show exception message on flexible staging (#801)

--- google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/cloud/CloudSdkAppEngineHelper.java
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/cloud/CloudSdkAppEngineHelper.java
@@ -114,7 +114,7 @@ public class CloudSdkAppEngineHelper implements AppEngineHelper {
     }
   }
 
-  @NotNull
+  @Nullable
   @Override
   public CancellableRunnable createDeployRunner(
       LoggingHandler loggingHandler,
@@ -122,10 +122,16 @@ public class CloudSdkAppEngineHelper implements AppEngineHelper {
       AppEngineDeploymentConfiguration deploymentConfiguration,
       DeploymentOperationCallback callback) {
 
-    if (source.getFile() == null
-        || !(source instanceof AppEngineDeployable)) {
-      callback.errorOccurred(GctBundle.message("appengine.deployment.source.not.found.error"));
+    if (!(source instanceof AppEngineDeployable)) {
+      callback.errorOccurred(GctBundle.message("appengine.deployment.invalid.source.error"));
       throw new RuntimeException("Invalid deployment source selected for deployment");
+    }
+
+    if (source.getFile() == null
+        || !source.getFile().exists()) {
+      callback.errorOccurred(GctBundle.message("appengine.deployment.source.not.found.error",
+          source.getFilePath()));
+      return null;
     }
 
     AppEngineEnvironment targetEnvironment = ((AppEngineDeployable) source).getEnvironment();
