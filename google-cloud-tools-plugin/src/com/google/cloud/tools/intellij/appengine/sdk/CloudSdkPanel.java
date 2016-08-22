enderakpil/gcloud-intellij

Adds rest of devappserver parameters to menus. Grabs SDK location from CloudSdkService.

google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/sdk/CloudSdkPanel.java
@@ -80,7 +80,7 @@ public class CloudSdkPanel {
 
   public boolean isModified() {
     return !Comparing.strEqual(getCloudSdkDirectory(),
-        CloudSdkService.getInstance().getCloudSdkHomePath());
+        CloudSdkService.getInstance().getCloudSdkHomePath().toString());
   }
 
   public void apply() throws ConfigurationException {
@@ -91,7 +91,7 @@ public class CloudSdkPanel {
   }
 
   public void reset() {
-    setCloudSdkDirectoryText(CloudSdkService.getInstance().getCloudSdkHomePath());
+    setCloudSdkDirectoryText(CloudSdkService.getInstance().getCloudSdkHomePath().toString());
   }
 
   public String getCloudSdkDirectory() {

GoogleCloudPlatform/cloud-code-intellij

google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/sdk/CloudSdkPanel.java
@@ -111,7 +111,7 @@ public class CloudSdkPanel {
 
   public boolean isModified() {
     return !Comparing.strEqual(getCloudSdkDirectory(),
-        CloudSdkService.getInstance().getSdkHomePath());
+        CloudSdkService.getInstance().getSdkHomePath().toString());
   }
 
   public void apply() throws ConfigurationException {
@@ -119,7 +119,7 @@ public class CloudSdkPanel {
   }
 
   public void reset() {
-    setCloudSdkDirectoryText(CloudSdkService.getInstance().getSdkHomePath());
+    setCloudSdkDirectoryText(CloudSdkService.getInstance().getSdkHomePath().toString());
   }
 
   public String getCloudSdkDirectory() {