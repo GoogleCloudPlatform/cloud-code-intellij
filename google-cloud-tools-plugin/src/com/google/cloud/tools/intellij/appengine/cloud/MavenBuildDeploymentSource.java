f394f960b01903644fc8010b8e88a488c7cff1ec
Etan Shaul <eshaul@google.com>
2016-08-31 13:41:47 -0400

    imports

--- google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/cloud/MavenBuildDeploymentSource.java
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/cloud/MavenBuildDeploymentSource.java
@@ -21,6 +21,8 @@ import com.intellij.openapi.project.Project;
 import com.intellij.remoteServer.configuration.deployment.DeploymentSourceType;
 import com.intellij.remoteServer.impl.configuration.deployment.ModuleDeploymentSourceImpl;
 
+import icons.MavenIcons;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.idea.maven.project.MavenProject;
@@ -30,7 +32,6 @@ import java.io.File;
 
 import javax.swing.Icon;
 
-import icons.MavenIcons;
 
 /**
  * A deployment source backed by the Maven build system.
