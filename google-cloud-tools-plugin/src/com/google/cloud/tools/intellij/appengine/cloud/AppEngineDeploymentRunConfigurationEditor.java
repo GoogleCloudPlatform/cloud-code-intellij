4323e319069c45c432a3e26b6c720cfc89a5f3e4
Etan Shaul <eshaul@google.com>
2016-09-08 16:41:42 -0400

    update link to pantheon ae versions page for manual promote

--- google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/cloud/AppEngineDeploymentRunConfigurationEditor.java
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/cloud/AppEngineDeploymentRunConfigurationEditor.java
@@ -108,7 +108,7 @@ public class AppEngineDeploymentRunConfigurationEditor extends
   private static final String LABEL_HREF_CLOSE_TAG = "</a>";
 
   private static final String PROMOTE_INFO_HREF_OPEN_TAG =
-      "<a href='https://cloud.google.com/appengine'>";
+      "<a href='https://pantheon.corp.google.com/appengine/versions'>";
   private static final String COST_WARNING_HREF_OPEN_TAG =
       "<a href='https://cloud.google.com/appengine/pricing'>";
 
