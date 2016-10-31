c138c49a9358c4db9a52cce32d1344a413bf8765
João André Martins <joaomartins@google.com>
2016-10-31 12:10:38 -0400

    Adds a Stackdriver facet.

---
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/debugger/facet/StackdriverFacetConfiguration.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.intellij.debugger.facet;
+
+import com.google.cloud.tools.intellij.debugger.facet.StackdriverFacetConfiguration.StackdriverProperties;
+
+import com.intellij.facet.FacetConfiguration;
+import com.intellij.facet.ui.FacetEditorContext;
+import com.intellij.facet.ui.FacetEditorTab;
+import com.intellij.facet.ui.FacetValidatorsManager;
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.util.InvalidDataException;
+import com.intellij.openapi.util.WriteExternalException;
+
+import org.jdom.Element;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Created by joaomartins on 10/27/16.
+ */
+public class StackdriverFacetConfiguration
+    implements FacetConfiguration, PersistentStateComponent<StackdriverProperties> {
+
+  private StackdriverProperties persistedProperties = new StackdriverProperties();
+
+  @Override
+  public FacetEditorTab[] createEditorTabs(FacetEditorContext editorContext,
+      FacetValidatorsManager validatorsManager) {
+    return new FacetEditorTab[]{
+        new StackdriverPanel(editorContext)
+    };
+  }
+
+  @Override
+  public void readExternal(Element element) throws InvalidDataException {
+  }
+
+  @Override
+  public void writeExternal(Element element) throws WriteExternalException {
+  }
+
+  @Nullable
+  @Override
+  public StackdriverProperties getState() {
+    return persistedProperties;
+  }
+
+  @Override
+  public void loadState(StackdriverProperties state) {
+    persistedProperties = state;
+  }
+
+  public static class StackdriverProperties {
+    private boolean generateSourceContext = true;
+    private boolean ignoreErrors = true;
+
+    public boolean isGenerateSourceContext() {
+      return generateSourceContext;
+    }
+
+    public void setGenerateSourceContext(boolean generateSourceContext) {
+      this.generateSourceContext = generateSourceContext;
+    }
+
+    public boolean isIgnoreErrors() {
+      return ignoreErrors;
+    }
+
+    public void setIgnoreErrors(boolean ignoreErrors) {
+      this.ignoreErrors = ignoreErrors;
+    }
+  }
+}
---
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/debugger/facet/StackdriverSupportProvider.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.intellij.debugger.facet;
+
+import com.intellij.facet.FacetManager;
+import com.intellij.facet.FacetType;
+import com.intellij.framework.FrameworkTypeEx;
+import com.intellij.framework.addSupport.FrameworkSupportInModuleConfigurable;
+import com.intellij.framework.addSupport.FrameworkSupportInModuleProvider;
+import com.intellij.ide.util.frameworkSupport.FrameworkSupportModel;
+import com.intellij.openapi.module.JavaModuleType;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleType;
+import com.intellij.openapi.roots.ModifiableModelsProvider;
+import com.intellij.openapi.roots.ModifiableRootModel;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.JComponent;
+
+/**
+ * Created by joaomartins on 10/27/16.
+ */
+public class StackdriverSupportProvider extends FrameworkSupportInModuleProvider {
+
+  @NotNull
+  @Override
+  public FrameworkTypeEx getFrameworkType() {
+    return StackdriverFrameworkType.getFrameworkType();
+  }
+
+  @NotNull
+  @Override
+  public FrameworkSupportInModuleConfigurable createConfigurable(
+      @NotNull FrameworkSupportModel model) {
+    return new StackdriverSupportConfigurable();
+  }
+
+  @Override
+  public boolean isEnabledForModuleType(@NotNull ModuleType moduleType) {
+    return moduleType instanceof JavaModuleType;
+  }
+
+  private class StackdriverSupportConfigurable extends FrameworkSupportInModuleConfigurable {
+
+    private StackdriverPanel stackdriverPanel = new StackdriverPanel();
+
+    @Nullable
+    @Override
+    public JComponent createComponent() {
+      return stackdriverPanel.createComponent();
+    }
+
+    @Override
+    public void addSupport(@NotNull Module module, @NotNull ModifiableRootModel rootModel,
+        @NotNull ModifiableModelsProvider modifiableModelsProvider) {
+      FacetType<StackdriverFacet, StackdriverFacetConfiguration> facetType =
+          new StackdriverFacetType();
+      StackdriverFacetConfiguration configuration = FacetManager.getInstance(module)
+          .addFacet(facetType, facetType.getPresentableName(), null /* underlying */)
+          .getConfiguration();
+      configuration.getState().setGenerateSourceContext(
+          stackdriverPanel.isGenerateSourceContextSelected());
+      configuration.getState().setIgnoreErrors(stackdriverPanel.isIgnoreErrorsSelected());
+    }
+  }
+}
