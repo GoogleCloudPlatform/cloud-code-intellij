5e0fd534c581e44fc3de6483c35106ecc3b4f127
João André Martins <joaomartins@google.com>
2016-12-01 12:19:37 -0500

    Dynamically add appengine-plugins-core.

---
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/compiler/StackdriverBuildProcessParametersProvider.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.intellij.compiler;
+
+import com.google.cloud.tools.appengine.cloudsdk.CloudSdk;
+
+import com.intellij.compiler.server.BuildProcessParametersProvider;
+import com.intellij.util.PathUtil;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Provides appengine-plugins-core library to the compilerServer dynamically, containing the
+ * most recent library version.
+ */
+public class StackdriverBuildProcessParametersProvider extends BuildProcessParametersProvider {
+
+  @NotNull
+  @Override
+  public List<String> getClassPath() {
+    List<String> classpath = new ArrayList<>();
+    classpath.add(PathUtil.getJarPathForClass(CloudSdk.class));
+    return classpath;
+  }
+}
