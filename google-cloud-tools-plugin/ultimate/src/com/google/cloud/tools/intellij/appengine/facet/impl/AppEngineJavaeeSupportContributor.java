2cf596e44f61f32250369d96f7d36ed8cc9a8f83
Etan Shaul <eshaul@google.com>
2017-02-06 15:53:44 -0500

    Force servlet 2.5 on new App Engine Standard apps

--- google-cloud-tools-plugin/ultimate/src/com/google/cloud/tools/intellij/appengine/facet/impl/AppEngineJavaeeSupportContributor.java
+++ google-cloud-tools-plugin/ultimate/src/com/google/cloud/tools/intellij/appengine/facet/impl/AppEngineJavaeeSupportContributor.java
@@ -19,14 +19,29 @@ package com.google.cloud.tools.intellij.appengine.facet.impl;
 import com.google.cloud.tools.intellij.appengine.facet.AppEngineStandardFacet;
 import com.google.cloud.tools.intellij.appengine.facet.AppEngineStandardWebIntegration;
 
+import com.intellij.javaee.model.xml.web.WebApp;
 import com.intellij.javaee.supportProvider.JavaeeFrameworkSupportContributionModel;
 import com.intellij.javaee.supportProvider.JavaeeFrameworkSupportContributor;
+import com.intellij.javaee.web.facet.WebFacet;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.startup.StartupManager;
 import com.intellij.packaging.artifacts.Artifact;
+import com.intellij.psi.xml.XmlAttribute;
+import com.intellij.psi.xml.XmlTag;
 
 /**
  * @author nik
  */
 public class AppEngineJavaeeSupportContributor extends JavaeeFrameworkSupportContributor {
+
+  private static final String SERVLET_NAMESPACE = "http://java.sun.com/xml/ns/javaee";
+  private static final String SERVLET_SCHEMA_URL
+      = "http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_2_5.xsd";
+
+  public AppEngineJavaeeSupportContributor() {
+
+  }
 
   @Override
   public void setupFrameworkSupport(JavaeeFrameworkSupportContributionModel model) {
@@ -34,6 +49,8 @@ public class AppEngineJavaeeSupportContributor extends JavaeeFrameworkSupportCon
     if (appEngineStandardFacet == null) {
       return;
     }
+
+    setWebXmlServletVersion(model.getModule());
 
     Artifact artifactToDeploy = model.getExplodedEarArtifact();
     if (artifactToDeploy == null) {
@@ -43,5 +60,36 @@ public class AppEngineJavaeeSupportContributor extends JavaeeFrameworkSupportCon
       AppEngineStandardWebIntegration.getInstance().setupRunConfigurations(
           artifactToDeploy, model.getProject(), model.getRunConfiguration());
     }
+  }
+
+  /**
+   * App Engine standard apps with the default runtime (Java7) require Servlet 2.5. This updates
+   * web.xml to the correct version.
+   */
+  private void setWebXmlServletVersion(Module module) {
+    StartupManager.getInstance(module.getProject()).runWhenProjectIsInitialized(() -> {
+      for (WebFacet webFacet : WebFacet.getInstances(module)) {
+        final WebApp webApp = webFacet.getRoot();
+        if (webApp != null) {
+          new WriteCommandAction.Simple(module.getProject()) {
+            @Override
+            protected void run() throws Throwable {
+              webApp.getVersion().setStringValue("2.5");
+
+              XmlTag webAppTag = webApp.getXmlTag();
+              XmlAttribute xmlns = webAppTag.getAttribute("xmlns");
+              XmlAttribute schemaLocation = webAppTag.getAttribute("xsi:schemaLocation");
+
+              if (xmlns != null) {
+                xmlns.setValue(SERVLET_NAMESPACE);
+              }
+              if (schemaLocation != null) {
+                schemaLocation.setValue(SERVLET_SCHEMA_URL);
+              }
+            }
+          }.execute();
+        }
+      }
+    });
   }
 }
