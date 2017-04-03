cb1bc59429f511b7c2d72ad3703cac39fd4fa86c
Etan Shaul <eshaul@google.com>
2017-04-03 17:28:25 -0400

    add comment

--- google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/project/DefaultAppEngineProjectService.java
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/project/DefaultAppEngineProjectService.java
@@ -345,7 +345,14 @@ public class DefaultAppEngineProjectService extends AppEngineProjectService {
         module);
 
     if (element != null) {
-      RenamePsiElementProcessor.DEFAULT.renameElement(element, "Dockerfile", UsageInfo.EMPTY_ARRAY, null);
+      // Remove the .docker extension to satisfy the Docker convention. This extension was added
+      // since the templating mechanism requires an extension or else a default template type of
+      // "java" will be assumed.
+      RenamePsiElementProcessor.DEFAULT.renameElement(
+          element,
+          "Dockerfile" /*newName*/,
+          UsageInfo.EMPTY_ARRAY,
+          null /*listener*/);
     }
   }
 
