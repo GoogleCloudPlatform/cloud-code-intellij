badce1c24b10935ecac98e93c0ed5b3320343df0
nbashirbello <nbashirbello@google.com>
2017-06-02 11:37:39 -0400

    Differentiate where the generation of the config files for flex are coming from

--- google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/project/AppEngineProjectService.java
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/project/AppEngineProjectService.java
@@ -140,15 +140,19 @@ public abstract class AppEngineProjectService {
   /**
    * Generates an app.yaml configuration file in the <@code>outputFolderPath</@code>. If an app.yaml
    * already exists it will not overwrite the file.
+   *
+   *  @return true if an app.yaml was successfully generated, false otherwise
    */
-  public abstract void generateAppYaml(FlexibleRuntime runtime, Module module, Path outputFolderPath);
+  public abstract boolean generateAppYaml(FlexibleRuntime runtime, Module module, Path outputFolderPath);
 
 
   /**
    * Generates a Dockerfile in the <@code>outputFolderPath</@code>. If a Dockerfile already exists
    * it will not overwrite the file.
+   *
+   * @return true if a Dockerfile was successfully generated, false otherwise
    */
-  public abstract void generateDockerfile(AppEngineFlexibleDeploymentArtifactType type,
+  public abstract boolean generateDockerfile(AppEngineFlexibleDeploymentArtifactType type,
       Module module, Path outputFolderPath);
 
   /**
