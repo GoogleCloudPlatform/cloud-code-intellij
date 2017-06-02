3a9809d82a90bedfe450accf19a99b34f9119960
Etan Shaul <eshaul@google.com>
2017-06-02 11:45:41 -0400

    creating shared deploy UX

---
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/cloud/AppEngineDeploymentEditor.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.intellij.appengine.cloud;
+
+import com.google.cloud.tools.intellij.resources.ProjectSelector;
+
+import javax.swing.JLabel;
+
+/**
+ * Common App Engine deployment configuration UI shared by flexible and standard.
+ */
+public class AppEngineDeploymentEditor {
+
+  private ProjectSelector projectSelector;
+  private JLabel environmentLabel;
+  private JLabel applicationInfoPanel;
+}
