e702cba35a0dff9f71a5b5562853362d144702b8
Etan Shaul <eshaul@google.com>
2017-07-25 12:51:02 -0400

    autopopulate issue template

--- google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/CloudToolsFeedbackAction.java
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/CloudToolsFeedbackAction.java
@@ -16,17 +16,37 @@
 
 package com.google.cloud.tools.intellij;
 
+import com.google.cloud.tools.appengine.api.AppEngineException;
+import com.google.cloud.tools.appengine.cloudsdk.CloudSdk;
 import com.google.cloud.tools.intellij.flags.PropertiesFileFlagReader;
 import com.google.cloud.tools.intellij.util.GctBundle;
+import com.google.common.net.UrlEscapers;
 import com.intellij.ide.BrowserUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.components.ServiceManager;
+import java.text.MessageFormat;
 
 /** Adds a Cloud Tools menu shortcut link to the GitHub issue creation page. */
 public class CloudToolsFeedbackAction extends AnAction {
 
   private static final String NEW_ISSUE_URL =
       new PropertiesFileFlagReader().getFlagString("plugin.github.new.issue.url");
+
+  // should be kept up to date with .github/ISSUE_TEMPLATE.md
+  private static final String BODY_TEMPLATE =
+      "(please ensure you are running the latest version of Cloud Tools for IntelliJ with _Help > Check for Updates_)\n"
+          + "- Cloud Tools for IntelliJ version: {0}\n"
+          + "- Google Cloud SDK version: {1}\n"
+          + "- OS: {2} {3}\n"
+          + "\n"
+          + "**What did you do?**\n"
+          + "\n"
+          + "**What did you expect to see?**\n"
+          + "\n"
+          + "**What did you see instead?**\n"
+          + "\n"
+          + "(screenshots are helpful)";
 
   public CloudToolsFeedbackAction() {
     super(
@@ -37,6 +57,30 @@ public class CloudToolsFeedbackAction extends AnAction {
 
   @Override
   public void actionPerformed(AnActionEvent event) {
-    BrowserUtil.browse(NEW_ISSUE_URL);
+    BrowserUtil.browse(formatUrl());
+  }
+
+  private static String formatUrl() {
+    String pluginVersion =
+        ServiceManager.getService(CloudToolsPluginInfoService.class).getPluginVersion();
+
+    String issueBody =
+        MessageFormat.format(
+            BODY_TEMPLATE,
+            pluginVersion,
+            getCloudSdkVersion(),
+            System.getProperty("os.name"),
+            System.getProperty("os.version"));
+
+    return NEW_ISSUE_URL + "?body=" + UrlEscapers.urlFormParameterEscaper().escape(issueBody);
+  }
+
+  private static String getCloudSdkVersion() {
+    try {
+      CloudSdk sdk = new CloudSdk.Builder().build();
+      return sdk.getVersion().toString();
+    } catch (AppEngineException aee) {
+      return "";
+    }
   }
 }
