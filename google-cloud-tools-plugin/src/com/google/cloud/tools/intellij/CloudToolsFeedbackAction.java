405626873e98e93f624c316e132a3246731ecc37
Etan Shaul <eshaul@google.com>
2017-07-31 12:41:28 -0400

    Adds a feedback link to the cloud tools menu (#1564)

---
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/CloudToolsFeedbackAction.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.intellij;
+
+import com.google.cloud.tools.appengine.api.AppEngineException;
+import com.google.cloud.tools.appengine.cloudsdk.CloudSdk;
+import com.google.cloud.tools.intellij.appengine.sdk.CloudSdkService;
+import com.google.cloud.tools.intellij.flags.PropertiesFileFlagReader;
+import com.google.cloud.tools.intellij.util.GctBundle;
+import com.google.common.net.UrlEscapers;
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.project.DumbAwareAction;
+import java.text.MessageFormat;
+
+/** Adds a Cloud Tools menu shortcut link to the GitHub issue creation page. */
+public class CloudToolsFeedbackAction extends DumbAwareAction {
+
+  private static final String NEW_ISSUE_URL =
+      new PropertiesFileFlagReader().getFlagString("plugin.github.new.issue.url");
+
+  // should be kept up to date with .github/ISSUE_TEMPLATE.md
+  private static final String BODY_TEMPLATE =
+      "(Please ensure you are running the latest version of Cloud Tools for IntelliJ with _Help > Check for Updates_)\n"
+          + "- Cloud Tools for IntelliJ version: {0}\n"
+          + "- Google Cloud SDK version: {1}\n"
+          + "- OS: {2} {3}\n"
+          + "\n"
+          + "**What did you do?**\n"
+          + "\n"
+          + "**What did you expect to see?**\n"
+          + "\n"
+          + "**What did you see instead?**\n"
+          + "\n"
+          + "(screenshots are helpful)\n"
+          + "_Feel free to deviate from this template as needed, especially if you are submitting a feature request._";
+
+  public CloudToolsFeedbackAction() {
+    super(
+        GctBundle.message("plugin.tools.menu.item.feedback.title"),
+        GctBundle.message("plugin.tools.menu.item.feedback.description"),
+        null /*icon*/);
+  }
+
+  @Override
+  public void actionPerformed(AnActionEvent event) {
+    BrowserUtil.browse(formatUrl());
+  }
+
+  private static String formatUrl() {
+    String pluginVersion =
+        ServiceManager.getService(CloudToolsPluginInfoService.class).getPluginVersion();
+
+    String issueBody =
+        MessageFormat.format(
+            BODY_TEMPLATE,
+            pluginVersion,
+            getCloudSdkVersion(),
+            System.getProperty("os.name"),
+            System.getProperty("os.version"));
+
+    return NEW_ISSUE_URL + "?body=" + UrlEscapers.urlFormParameterEscaper().escape(issueBody);
+  }
+
+  private static String getCloudSdkVersion() {
+    try {
+      CloudSdk sdk =
+          new CloudSdk.Builder().sdkPath(CloudSdkService.getInstance().getSdkHomePath()).build();
+      return sdk.getVersion().toString();
+    } catch (AppEngineException aee) {
+      return "";
+    }
+  }
+}
---
+++ google-cloud-tools-plugin/testSrc/com/google/cloud/tools/intellij/CloudToolsFeedbackActionTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.intellij;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.cloud.tools.intellij.appengine.sdk.CloudSdkService;
+import com.google.cloud.tools.intellij.testing.CloudToolsRule;
+import com.google.cloud.tools.intellij.testing.TestFile;
+import com.google.cloud.tools.intellij.testing.TestService;
+import com.google.common.net.UrlEscapers;
+import com.intellij.ide.browsers.BrowserLauncher;
+import java.io.File;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+
+/** Tests for {@link CloudToolsFeedbackAction}. */
+public class CloudToolsFeedbackActionTest {
+
+  @Rule public final CloudToolsRule cloudToolsRule = new CloudToolsRule(this);
+
+  @Mock @TestService private CloudSdkService sdkService;
+  @Mock @TestService private BrowserLauncher browserLauncher;
+
+  @TestFile(name = "testCloudSdk")
+  private File missingCloudSdk;
+
+  private CloudToolsFeedbackAction feedbackAction;
+
+  private static ArgumentCaptor<String> urlArg = ArgumentCaptor.forClass(String.class);
+
+  @Before
+  public void setUp() {
+    feedbackAction = new CloudToolsFeedbackAction();
+  }
+
+  @Test
+  public void testDisplayableSdkVersion_isEmpty_whenSdkIsInvalid() {
+    when(sdkService.getSdkHomePath()).thenReturn(missingCloudSdk.toPath());
+
+    feedbackAction.actionPerformed(null /*event*/);
+    verify(browserLauncher).browse(urlArg.capture(), eq(null));
+
+    String expectedEmptySdkVersionMessage = "- Google Cloud SDK version: \n";
+    assertTrue(urlContainsMessage(expectedEmptySdkVersionMessage));
+  }
+
+  @Test
+  public void testDisplayableOs() {
+    feedbackAction.actionPerformed(null /*event*/);
+    verify(browserLauncher).browse(urlArg.capture(), eq(null));
+
+    String expectedOsMessage =
+        "OS: " + System.getProperty("os.name") + " " + System.getProperty("os.version") + "\n";
+    assertTrue(urlContainsMessage(expectedOsMessage));
+  }
+
+  private static boolean urlContainsMessage(String message) {
+    return urlArg.getValue().contains(UrlEscapers.urlFormParameterEscaper().escape(message));
+  }
+}
