f828a1ba1a589399bc6f3ff09e9fe3c9cfac206b
Nate Kibler <nkibler@google.com>
2017-08-01 11:38:24 -0400

    Removes the SoftReference logic when loading ResourceBundles. (#1569)

--- google-account-plugin/src/com/google/cloud/tools/intellij/login/util/TrackerMessageBundle.java
+++ google-account-plugin/src/com/google/cloud/tools/intellij/login/util/TrackerMessageBundle.java
@@ -17,44 +17,26 @@
 package com.google.cloud.tools.intellij.login.util;
 
 import com.intellij.CommonBundle;
-
+import java.util.ResourceBundle;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
-import java.lang.ref.Reference;
-import java.lang.ref.SoftReference;
-import java.util.ResourceBundle;
+/** Bundle class to get usage tracker messages from resources/messages. */
+public final class TrackerMessageBundle {
 
-/**
- * Bundle class to get usage tracker messages from resources/messages.
- */
-public class TrackerMessageBundle {
+  @NonNls private static final String BUNDLE_NAME = "messages.UsageTrackerBundle";
 
-  @NonNls
-  private static final String BUNDLE_NAME = "messages.UsageTrackerBundle";
-  private static Reference<ResourceBundle> bundleReference;
+  /** Prevents instantiation. */
+  private TrackerMessageBundle() {}
 
-  private static ResourceBundle getBundle() {
-    ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(
-        bundleReference);
-    if (bundle == null) {
-      bundle = ResourceBundle.getBundle(BUNDLE_NAME);
-      bundleReference = new SoftReference<ResourceBundle>(bundle);
-    }
-    return bundle;
+  public static String message(
+      @NotNull @PropertyKey(resourceBundle = BUNDLE_NAME) String key, @NotNull Object... params) {
+    return CommonBundle.message(ResourceBundle.getBundle(BUNDLE_NAME), key, params);
   }
 
-  private TrackerMessageBundle() {
-  }
-
-  public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE_NAME) String key,
-      @NotNull Object... params) {
-    return CommonBundle.message(getBundle(), key, params);
-  }
-
-  public static String getString(@NotNull @PropertyKey(resourceBundle = BUNDLE_NAME) String key,
-      @NotNull Object... params) {
+  public static String getString(
+      @NotNull @PropertyKey(resourceBundle = BUNDLE_NAME) String key, @NotNull Object... params) {
     return message(key, params);
   }
 }
