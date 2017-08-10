9969edf27a177d66e67b4f43b1f143f9f1c240bc
Nate Kibler <nkibler@google.com>
2017-08-10 14:37:10 -0400

    Fixes EAP test failure.

--- google-cloud-tools-plugin/testSrc/com/google/cloud/tools/intellij/CloudToolsFeedbackActionTest.java
+++ google-cloud-tools-plugin/testSrc/com/google/cloud/tools/intellij/CloudToolsFeedbackActionTest.java
@@ -16,8 +16,11 @@
 
 package com.google.cloud.tools.intellij;
 
-import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -27,15 +30,21 @@ import com.google.cloud.tools.intellij.testing.TestFile;
 import com.google.cloud.tools.intellij.testing.TestService;
 import com.google.common.net.UrlEscapers;
 import com.intellij.ide.browsers.BrowserLauncher;
+import com.intellij.ide.browsers.WebBrowser;
+import com.intellij.openapi.project.Project;
 import java.io.File;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.mockito.Mock;
 
 /** Tests for {@link CloudToolsFeedbackAction}. */
-public class CloudToolsFeedbackActionTest {
+@RunWith(JUnit4.class)
+public final class CloudToolsFeedbackActionTest {
 
   @Rule public final CloudToolsRule cloudToolsRule = new CloudToolsRule(this);
 
@@ -47,11 +56,10 @@ public class CloudToolsFeedbackActionTest {
 
   private CloudToolsFeedbackAction feedbackAction;
 
-  private static ArgumentCaptor<String> urlArg = ArgumentCaptor.forClass(String.class);
-
   @Before
-  public void setUp() {
+  public void setUp() throws Exception {
     feedbackAction = new CloudToolsFeedbackAction();
+    maybeStubBrowserLauncher();
   }
 
   @Test
@@ -59,23 +67,54 @@ public class CloudToolsFeedbackActionTest {
     when(sdkService.getSdkHomePath()).thenReturn(missingCloudSdk.toPath());
 
     feedbackAction.actionPerformed(null /*event*/);
-    verify(browserLauncher).browse(urlArg.capture(), eq(null));
 
-    String expectedEmptySdkVersionMessage = "- Google Cloud SDK version: \n";
-    assertTrue(urlContainsMessage(expectedEmptySdkVersionMessage));
+    String expected = "- Google Cloud SDK version: \n";
+    verify(browserLauncher).browse(urlContains(expected), eq(null), eq(null));
   }
 
   @Test
   public void testDisplayableOs() {
     feedbackAction.actionPerformed(null /*event*/);
-    verify(browserLauncher).browse(urlArg.capture(), eq(null));
 
-    String expectedOsMessage =
-        "OS: " + System.getProperty("os.name") + " " + System.getProperty("os.version") + "\n";
-    assertTrue(urlContainsMessage(expectedOsMessage));
+    String expected =
+        String.format(
+            "OS: %s %s\n", System.getProperty("os.name"), System.getProperty("os.version"));
+    verify(browserLauncher).browse(urlContains(expected), eq(null), eq(null));
   }
 
-  private static boolean urlContainsMessage(String message) {
-    return urlArg.getValue().contains(UrlEscapers.urlFormParameterEscaper().escape(message));
+  /**
+   * Stubs the {@link BrowserLauncher#browse(String, WebBrowser)} to call the {@link
+   * BrowserLauncher#browse(String, WebBrowser, Project)} method with a {@code null} {@link Project}
+   * if the method is non-final.
+   *
+   * <p>This required for compatibility reasons between the previous non-Kotlin source for {@link
+   * BrowserLauncher} and the new Kotlin source.
+   *
+   * <p><b>Explanation:</b> The non-Kotlin source declares {@link BrowserLauncher#browse(String,
+   * WebBrowser)} as abstract; the Kotlin source declares this method as final, whose implementation
+   * calls into the {@link BrowserLauncher#browse(String, WebBrowser, Project)} method. Since
+   * Mockito cannot stub final methods, the real implementation will be invoked if the underlying
+   * source is the Kotlin version.
+   *
+   * <p>To support verifying the {@link BrowserLauncher#browse} method was called by the class under
+   * test, the tests verify the 3-parameter method was called and this method will forward
+   * invocations of the 2-parameter method if needed.
+   */
+  private void maybeStubBrowserLauncher() throws NoSuchMethodException {
+    Method browseMethod = BrowserLauncher.class.getMethod("browse", String.class, WebBrowser.class);
+    if (!Modifier.isFinal(browseMethod.getModifiers())) {
+      doAnswer(
+              invocation -> {
+                String url = (String) invocation.getArguments()[0];
+                browserLauncher.browse(url, null, null);
+                return null;
+              })
+          .when(browserLauncher)
+          .browse(anyString(), any());
+    }
+  }
+
+  private static String urlContains(String unescapedMessage) {
+    return contains(UrlEscapers.urlFormParameterEscaper().escape(unescapedMessage));
   }
 }
