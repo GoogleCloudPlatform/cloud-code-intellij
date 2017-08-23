db733b556fa69b1aa71eac0c7f1a7974a0392bfb
Etan Shaul <eshaul@google.com>
2017-08-23 12:00:35 -0400

    Google Cloud Storage integration setup (#1615)

---
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/gcs/GcsToolWindowPanel.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.intellij.gcs;
+
+import com.google.cloud.tools.intellij.util.GctBundle;
+import com.intellij.icons.AllIcons.Actions;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.ActionToolbar;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.project.DumbAwareAction;
+import com.intellij.openapi.ui.SimpleToolWindowPanel;
+import com.intellij.util.ui.JBUI;
+import javax.swing.JPanel;
+
+/** Defines a Google Cloud Storage tool panel. */
+final class GcsToolWindowPanel extends SimpleToolWindowPanel {
+
+  private static final String GCS_PANEL_TOOLBAR_ACTION = "GcsPanelToolbar";
+
+  GcsToolWindowPanel() {
+    super(true /*vertical*/, true /*borderless*/);
+
+    setToolbar(createToolbar());
+  }
+
+  private JPanel createToolbar() {
+    DefaultActionGroup group = new DefaultActionGroup();
+    group.add(new RefreshAction());
+
+    ActionToolbar actionToolBar =
+        ActionManager.getInstance()
+            .createActionToolbar(GCS_PANEL_TOOLBAR_ACTION, group, true /*horizontal*/);
+
+    return JBUI.Panels.simplePanel(actionToolBar.getComponent());
+  }
+
+  private static final class RefreshAction extends DumbAwareAction {
+    RefreshAction() {
+      super(
+          GctBundle.message("gcs.panel.toolbar.refresh.hover.text"),
+          GctBundle.message("gcs.panel.toolbar.refresh.hover.description"),
+          Actions.Refresh);
+    }
+
+    @Override
+    public void actionPerformed(AnActionEvent event) {
+      // TODO refresh bucket list
+    }
+  }
+}
---
+++ google-cloud-tools-plugin/testSrc/com/google/cloud/tools/intellij/gcs/GcsToolWindowPanelTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.intellij.gcs;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.google.cloud.tools.intellij.testing.CloudToolsRule;
+import com.intellij.openapi.application.ApplicationManager;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+/** Tests for {@link GcsToolWindowPanel}. */
+public class GcsToolWindowPanelTest {
+  @Rule public final CloudToolsRule cloudToolsRule = new CloudToolsRule(this);
+
+  private GcsToolWindowPanel gcsPanel;
+
+  @Before
+  public void setUp() throws Exception {
+    ApplicationManager.getApplication().invokeAndWait(() -> gcsPanel = new GcsToolWindowPanel());
+  }
+
+  @Test
+  public void testPanelInitialization() {
+    assertTrue(gcsPanel.isVisible());
+    assertTrue(gcsPanel.isToolbarVisible());
+  }
+}
