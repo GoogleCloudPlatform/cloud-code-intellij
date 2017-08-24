66cd80fe75fa1660b1b9a29c221610f56be1913e
Etan Shaul <eshaul@google.com>
2017-08-24 12:54:24 -0400

    oops

--- google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/gcs/GcsBucketPanel.java
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/gcs/GcsBucketPanel.java
@@ -29,6 +29,8 @@ import com.google.common.collect.Iterators;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.ui.DocumentAdapter;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.concurrent.Future;
 import javax.swing.DefaultListModel;
 import javax.swing.JLabel;
@@ -69,6 +71,15 @@ final class GcsBucketPanel {
                 refresh();
               }
             });
+
+    bucketList.addMouseListener(new MouseAdapter() {
+      @Override
+      public void mouseClicked(MouseEvent event) {
+        if (event.getClickCount() == 2) {
+          loadBucketContents();
+        }
+      }
+    });
   }
 
   void refresh() {
@@ -115,12 +126,12 @@ final class GcsBucketPanel {
   }
 
   private void loadAndDisplayBuckets(String projectId, Credential credential) {
-    bucketListModel.clear();
-    notificationLabel.setText(GctBundle.message("gcs.panel.bucket.listing.loading.text"));
-
-    if (bucketLoadExecution != null && bucketLoadExecution.isDone()) {
+    if (bucketLoadExecution != null && !bucketLoadExecution.isDone()) {
       return;
     }
+
+    bucketListModel.clear();
+    notificationLabel.setText(GctBundle.message("gcs.panel.bucket.listing.loading.text"));
 
     bucketLoadExecution =
         ApplicationManager.getApplication()
@@ -148,6 +159,10 @@ final class GcsBucketPanel {
                         GctBundle.message("gcs.panel.bucket.listing.error.loading.buckets"));
                   }
                 });
+  }
+
+  private void loadBucketContents() {
+
   }
 
   private void showNotificationPanel(boolean show) {
