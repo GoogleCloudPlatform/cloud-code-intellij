353c52d76911dc8e376ca1cc5c49685993cbc8dd
Nate Kibler <nkibler@google.com>
2017-11-09 12:50:24 -0500

    Makes checkboxes match mocked UI in design doc.

--- google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/apis/GoogleCloudApiSelectorPanel.java
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/apis/GoogleCloudApiSelectorPanel.java
@@ -18,16 +18,30 @@ package com.google.cloud.tools.intellij.apis;
 
 import com.google.cloud.tools.libraries.CloudLibraries;
 import com.google.cloud.tools.libraries.json.CloudLibrary;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.ui.JBSplitter;
 import com.intellij.ui.components.JBCheckBox;
 import com.intellij.ui.components.JBScrollPane;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.util.Comparator;
+import java.util.function.Consumer;
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
 
 /** The form-bound class for the Cloud API selector panel. */
 final class GoogleCloudApiSelectorPanel {
+
+  private static final Logger logger = Logger.getInstance(GoogleCloudApiSelectorPanel.class);
 
   private JPanel panel;
   private JBScrollPane leftScrollPane;
@@ -51,6 +65,8 @@ final class GoogleCloudApiSelectorPanel {
     splitter.setFirstComponent(leftScrollPane);
     splitter.setSecondComponent(rightScrollPane);
 
+    detailsForm = new GoogleCloudApiDetailsPanel();
+
     checkboxPanel = new JPanel();
     checkboxPanel.setLayout(new BoxLayout(checkboxPanel, BoxLayout.PAGE_AXIS));
     try {
@@ -59,7 +75,7 @@ final class GoogleCloudApiSelectorPanel {
           .sorted(Comparator.comparing(CloudLibrary::getName))
           .forEach(this::addLibraryCheckbox);
     } catch (IOException e) {
-      e.printStackTrace();
+      logger.error(e);
     }
   }
 
@@ -69,8 +85,67 @@ final class GoogleCloudApiSelectorPanel {
    * @param library the {@link CloudLibrary} to add the checkbox for
    */
   private void addLibraryCheckbox(CloudLibrary library) {
-    JBCheckBox checkBox = new JBCheckBox(library.getName());
-    checkBox.addActionListener(event -> detailsForm.setCloudLibrary(library));
-    checkboxPanel.add(checkBox);
+    LibraryCheckboxPanel panel = new LibraryCheckboxPanel(library, detailsForm::setCloudLibrary);
+    checkboxPanel.add(panel);
+  }
+
+  /** The panel that represents a checkbox for a {@link CloudLibrary}. */
+  private static final class LibraryCheckboxPanel extends JPanel {
+
+    private static final int BORDER_THICKNESS = 2;
+    private static final Color FOCUS_BORDER_COLOR = UIManager.getColor("Button.focus");
+    private static final Border FOCUSED_BORDER =
+        BorderFactory.createLineBorder(FOCUS_BORDER_COLOR, BORDER_THICKNESS);
+    private static final Border EMPTY_BORDER =
+        BorderFactory.createEmptyBorder(
+            BORDER_THICKNESS, BORDER_THICKNESS, BORDER_THICKNESS, BORDER_THICKNESS);
+
+    private final CloudLibrary library;
+    private final Consumer<CloudLibrary> focusHandler;
+
+    /**
+     * Returns a new instance for the given {@link CloudLibrary} and selected handler.
+     *
+     * @param library the {@link CloudLibrary} that this checkbox panel represents
+     * @param focusHandler a {@link Consumer} that accepts the {@link CloudLibrary} when the panel
+     *     is focused
+     */
+    LibraryCheckboxPanel(CloudLibrary library, Consumer<CloudLibrary> focusHandler) {
+      super(new FlowLayout(FlowLayout.LEFT));
+      this.library = library;
+      this.focusHandler = focusHandler;
+      init();
+    }
+
+    /** Initializes the UI for this panel. */
+    void init() {
+      setBorder(EMPTY_BORDER);
+      addMouseListener(
+          new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+              e.getComponent().requestFocusInWindow();
+            }
+          });
+      addFocusListener(
+          new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+              ((JPanel) e.getComponent()).setBorder(FOCUSED_BORDER);
+              focusHandler.accept(library);
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+              ((JPanel) e.getComponent()).setBorder(EMPTY_BORDER);
+            }
+          });
+
+      JBCheckBox checkbox = new JBCheckBox();
+      checkbox.setFocusable(false);
+      checkbox.addActionListener(e -> checkbox.getParent().requestFocusInWindow());
+      add(checkbox);
+      add(new JLabel(library.getName()));
+    }
   }
 }
