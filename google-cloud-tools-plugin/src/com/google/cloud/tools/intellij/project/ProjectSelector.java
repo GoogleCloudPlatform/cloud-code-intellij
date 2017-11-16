9b2789e31c2eaf0ac6e2481aaba5aa76dbad2d41
Ivan Portyankin <ivan.portyankin@gmail.com>
2017-11-16 10:42:23 -0500

    prepare initial class/package structure for refactored project selector.

--- google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/project/ProjectSelector.java
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/project/ProjectSelector.java
@@ -1,5 +1,48 @@
+/*
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.cloud.tools.intellij.project;
 
-public class ProjectSelector {
+import com.google.cloud.tools.intellij.login.IntellijGoogleLoginService;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * ProjectSelector allows the user to select a GCP project id. It calls into {@link
+ * IntellijGoogleLoginService} to get the set of credentialed users and then into
+ * resource manager to get the set of projects. */
+public class ProjectSelector {
+  private final List<ProjectSelectionListener> projectSelectionListeners = new ArrayList<>();
+
+  /**
+   * Adds a {@link ProjectSelectionListener} to this class's internal list of listeners. All
+   * ProjectSelectionListeners are notified when the selection is changed to a valid project.
+   *
+   * @param projectSelectionListener the listener to add
+   */
+  public void addProjectSelectionListener(ProjectSelectionListener projectSelectionListener) {
+    projectSelectionListeners.add(projectSelectionListener);
+  }
+
+  /**
+   * Removes a {@link ProjectSelectionListener} from this class's internal list of listeners.
+   *
+   * @param projectSelectionListener the listener to remove
+   */
+  public void removeProjectSelectionListener(ProjectSelectionListener projectSelectionListener) {
+    projectSelectionListeners.remove(projectSelectionListener);
+  }
 }
