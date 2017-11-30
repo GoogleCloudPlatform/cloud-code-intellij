5116944c40d1ad08b46531a4b9ec1691950f97d9
Nate Kibler <nkibler@google.com>
2017-11-30 14:59:40 -0500

    Adds notifications to user for added/ignored libraries.

--- google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/apis/CloudLibraryDependencyWriter.java
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/apis/CloudLibraryDependencyWriter.java
@@ -16,16 +16,25 @@
 
 package com.google.cloud.tools.intellij.apis;
 
+import com.google.cloud.tools.intellij.flags.PropertiesFileFlagReader;
+import com.google.cloud.tools.intellij.ui.GoogleCloudToolsIcons;
+import com.google.cloud.tools.intellij.util.GctBundle;
 import com.google.cloud.tools.libraries.json.CloudLibrary;
 import com.google.cloud.tools.libraries.json.CloudLibraryClientMavenCoordinates;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationDisplayType;
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.NotificationType;
 import com.intellij.openapi.application.Result;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.intellij.util.xml.DomUtil;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.idea.maven.dom.MavenDomUtil;
 import org.jetbrains.idea.maven.dom.model.MavenDomDependency;
@@ -36,6 +45,14 @@ import org.jetbrains.idea.maven.project.MavenProjectsManager;
 
 /** A helper class that adds dependencies of Cloud libraries to a given module. */
 final class CloudLibraryDependencyWriter {
+
+  private static final NotificationGroup NOTIFICATION_GROUP =
+      new NotificationGroup(
+          new PropertiesFileFlagReader().getFlagString("notifications.plugin.groupdisplayid"),
+          NotificationDisplayType.BALLOON,
+          true,
+          null,
+          GoogleCloudToolsIcons.CLOUD);
 
   /** Prevents instantiation. */
   private CloudLibraryDependencyWriter() {}
@@ -56,41 +73,63 @@ final class CloudLibraryDependencyWriter {
     }
 
     Project project = module.getProject();
-    MavenProjectsManager projectsManager = MavenProjectsManager.getInstance(module.getProject());
-    MavenProject mavenProject = projectsManager.findProject(module);
-    if (mavenProject == null) {
+    if (MavenProjectsManager.getInstance(project).isMavenizedModule(module)) {
+      addLibrariesToMavenModule(libraries, module);
+    } else {
       // TODO(nkibler): Handle non-Maven projects.
-      return;
     }
+  }
 
+  /**
+   * Adds the given set of {@link CloudLibrary CloudLibraries} to the given Maven {@link Module}.
+   *
+   * <p>If the given {@link Module} is not a Maven module, this method may throw an undefined {@link
+   * RuntimeException}.
+   *
+   * @param libraries the set of {@link CloudLibrary CloudLibraries} to add
+   * @param module the Maven {@link Module} to add the libraries to
+   */
+  private static void addLibrariesToMavenModule(
+      @NotNull Set<CloudLibrary> libraries, @NotNull Module module) {
+    Project project = module.getProject();
+    MavenProject mavenProject = MavenProjectsManager.getInstance(project).findProject(module);
     MavenDomProjectModel model =
         MavenDomUtil.getMavenDomProjectModel(project, mavenProject.getFile());
-    if (model == null) {
-      // TODO(nkibler): Handle this error-state, maybe a warning?
-      return;
-    }
 
     new WriteCommandAction(project, DomUtil.getFile(model)) {
       @Override
       protected void run(@NotNull Result result) throws Throwable {
         List<MavenDomDependency> dependencies = model.getDependencies().getDependencies();
-        // TODO(nkibler): Inform the user when libraries were ignored because of existing
-        // dependencies, maybe a warning?
-        libraries
-            .stream()
-            .map(CloudLibraryUtils::getFirstJavaClientMavenCoordinates)
-            .filter(Optional::isPresent)
-            .map(Optional::get)
-            .map(CloudLibraryDependencyWriter::toMavenId)
-            .filter(mavenId -> isMavenIdNotInDependencyList(mavenId, dependencies))
-            .forEach(
-                mavenId -> MavenDomUtil.createDomDependency(model, /* editor= */ null, mavenId));
+        Map<Boolean, List<MavenId>> mavenIdsMap =
+            libraries
+                .stream()
+                .map(CloudLibraryUtils::getFirstJavaClientMavenCoordinates)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .map(CloudLibraryDependencyWriter::toMavenId)
+                .collect(
+                    Collectors.partitioningBy(
+                        mavenId -> isMavenIdInDependencyList(mavenId, dependencies)));
+
+        // The MavenIds in the "true" list are already in the pom.xml, so we don't duplicate them
+        // and warn the user that they weren't added.
+        List<MavenId> ignoredMavenIds = mavenIdsMap.get(true);
+        notifyIgnoredDependencies(ignoredMavenIds, project);
+
+        // The MavenIds in the "false" list are not in the pom.xml, so we add them and notify the
+        // user when it's complete.
+        List<MavenId> newMavenIds = mavenIdsMap.get(false);
+        if (!newMavenIds.isEmpty()) {
+          newMavenIds.forEach(
+              mavenId -> MavenDomUtil.createDomDependency(model, /* editor= */ null, mavenId));
+          notifyAddedDependencies(newMavenIds, project);
+        }
       }
     }.execute();
   }
 
   /**
-   * Returns {@code true} if the given {@link MavenId} is not in the given list of {@link
+   * Returns {@code true} if the given {@link MavenId} is in the given list of {@link
    * MavenDomDependency dependencies}.
    *
    * <p>Note that equality is tested via matching group IDs and artifact IDs. Equality of versions
@@ -101,11 +140,11 @@ final class CloudLibraryDependencyWriter {
    * @param dependencies the list of {@link MavenDomDependency} objects that currently exist in the
    *     DOM model
    */
-  private static boolean isMavenIdNotInDependencyList(
+  private static boolean isMavenIdInDependencyList(
       MavenId mavenId, List<MavenDomDependency> dependencies) {
     return dependencies
         .stream()
-        .noneMatch(
+        .anyMatch(
             dependency ->
                 mavenId.equals(
                     dependency.getGroupId().getStringValue(),
@@ -124,5 +163,65 @@ final class CloudLibraryDependencyWriter {
         mavenCoordinates.getGroupId(),
         mavenCoordinates.getArtifactId(),
         mavenCoordinates.getVersion());
+  }
+
+  /**
+   * Notifies the user that the given list of {@link MavenId MavenIds} were ignored when attempting
+   * to add them to their pom.xml because they already exist.
+   *
+   * <p>The notification shown has a {@link NotificationType#WARNING} type.
+   *
+   * @param ignoredMavenIds the list of {@link MavenId MavenIds} that were ignored
+   * @param project the {@link Project} that was affected
+   */
+  private static void notifyIgnoredDependencies(List<MavenId> ignoredMavenIds, Project project) {
+    if (ignoredMavenIds.isEmpty()) {
+      return;
+    }
+
+    String title = GctBundle.message("cloud.libraries.depwriter.maven.ignored.deps.title");
+    String mavenIdString = joinMavenIds(ignoredMavenIds);
+    String message =
+        GctBundle.message("cloud.libraries.depwriter.maven.ignored.deps.message", mavenIdString);
+    Notification notification =
+        NOTIFICATION_GROUP.createNotification(
+            title, /* subtitle= */ null, message, NotificationType.WARNING);
+    notification.notify(project);
+  }
+
+  /**
+   * Notifies the user that the given list of {@link MavenId MavenIds} were added to their pom.xml.
+   *
+   * <p>The notification shown has an {@link NotificationType#INFORMATION} type.
+   *
+   * @param addedMavenIds the list of {@link MavenId MavenIds} that were added to the user's pom.xml
+   * @param project the {@link Project} that was affected
+   */
+  private static void notifyAddedDependencies(List<MavenId> addedMavenIds, Project project) {
+    if (addedMavenIds.isEmpty()) {
+      return;
+    }
+
+    String title = GctBundle.message("cloud.libraries.depwriter.maven.added.deps.title");
+    String mavenIdString = joinMavenIds(addedMavenIds);
+    String message =
+        GctBundle.message("cloud.libraries.depwriter.maven.added.deps.message", mavenIdString);
+    Notification notification =
+        NOTIFICATION_GROUP.createNotification(
+            title, /* subtitle= */ null, message, NotificationType.INFORMATION);
+    notification.notify(project);
+  }
+
+  /**
+   * Joins the given list of {@link MavenId MavenIds} into a human-readable string.
+   *
+   * @param mavenIds the list of {@link MavenId MavenIds} to join
+   */
+  private static String joinMavenIds(List<MavenId> mavenIds) {
+    return mavenIds
+        .stream()
+        .map(MavenId::getDisplayString)
+        .map(string -> "- " + string)
+        .collect(Collectors.joining("\n"));
   }
 }
