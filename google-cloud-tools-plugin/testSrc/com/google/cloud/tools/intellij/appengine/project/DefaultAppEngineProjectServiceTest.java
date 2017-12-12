b05e71427287873d9a1eded8011e919e1873614a
Etan Shaul <eshaul@google.com>
2017-12-12 12:28:28 -0500

    rolling back changes to DefaultAppEngineProjectServiceTest

--- google-cloud-tools-plugin/testSrc/com/google/cloud/tools/intellij/appengine/project/DefaultAppEngineProjectServiceTest.java
+++ google-cloud-tools-plugin/testSrc/com/google/cloud/tools/intellij/appengine/project/DefaultAppEngineProjectServiceTest.java
@@ -16,24 +16,15 @@
 
 package com.google.cloud.tools.intellij.appengine.project;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.fail;
-
 import com.google.cloud.tools.intellij.appengine.cloud.AppEngineEnvironment;
 import com.google.cloud.tools.intellij.appengine.cloud.flexible.AppEngineFlexibleDeploymentArtifactType;
 import com.google.cloud.tools.intellij.appengine.cloud.standard.AppEngineStandardRuntime;
 import com.google.cloud.tools.intellij.appengine.facet.flexible.AppEngineFlexibleFacetType;
 import com.google.cloud.tools.intellij.appengine.facet.standard.AppEngineStandardFacetType;
 import com.google.cloud.tools.intellij.appengine.project.AppEngineProjectService.FlexibleRuntime;
-import com.google.cloud.tools.intellij.testing.CloudToolsRule;
-import com.google.cloud.tools.intellij.testing.TestFile;
-import com.google.cloud.tools.intellij.testing.TestFixture;
-import com.google.cloud.tools.intellij.testing.TestModule;
+
 import com.intellij.appengine.AppEngineCodeInsightTestCase;
 import com.intellij.facet.FacetManager;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.Result;
 import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.module.Module;
@@ -42,97 +33,68 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess;
 import com.intellij.psi.PsiManager;
 import com.intellij.psi.xml.XmlFile;
-import com.intellij.testFramework.fixtures.IdeaProjectTestFixture;
+import com.intellij.testFramework.PlatformTestCase;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.Assert;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import org.jetbrains.annotations.NotNull;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
 
 /**
  * Unit tests for {@link DefaultAppEngineProjectService}
  */
-public class DefaultAppEngineProjectServiceTest {
-  @Rule public final CloudToolsRule cloudToolsRule = new CloudToolsRule(this);
-
-  @TestFixture private IdeaProjectTestFixture testFixture;
-
-  private @TestModule(facetTypeId = AppEngineFlexibleFacetType.STRING_ID) Module flexModule;
-  private @TestModule(facetTypeId = AppEngineStandardFacetType.STRING_ID) Module standardModule;
-
-
-  private @TestFile(name = "app.yaml", contents = "   runtime :    custom ") File
-      appYamlCustomRuntime;
-
-  private @TestFile(name = "app.yaml", contents = "runtime: custom\nenv_variables:\n  'DBG_ENAB")
-  File appYamlCustomRuntimeWithEnvVars;
+public class DefaultAppEngineProjectServiceTest extends PlatformTestCase {
 
   private DefaultAppEngineProjectService appEngineProjectService;
 
-  @Before
-  public void setUp() throws Exception {
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
     // Fixes https://youtrack.jetbrains.com/issue/IDEA-129297. Only occurs in Jenkins.
-//    VfsRootAccess.allowRootAccess(System.getProperty("user.dir"));
+    VfsRootAccess.allowRootAccess(System.getProperty("user.dir"));
     appEngineProjectService = new DefaultAppEngineProjectService();
   }
 
-  @Test
   public void testGetAppEngineArtifactEnvironment_Standard() {
+    Module module = createModule("myModule");
+    addAppEngineStandardFacet(module);
+
     assertEquals(AppEngineEnvironment.APP_ENGINE_STANDARD,
-        appEngineProjectService.getModuleAppEngineEnvironment(standardModule).get());
+        appEngineProjectService.getModuleAppEngineEnvironment(module).get());
   }
 
-  @Test
   public void testGetAppEngineArtifactEnvironment_Flexible() {
+    Module module = createModule("myModule");
+    addAppEngineFlexibleFacet(module);
+
     assertEquals(AppEngineEnvironment.APP_ENGINE_FLEX,
-        appEngineProjectService.getModuleAppEngineEnvironment(flexModule).get());
+        appEngineProjectService.getModuleAppEngineEnvironment(module).get());
   }
 
-  @Test
   public void testGetAppEngineStandardDeclaredRuntime_NullArg() {
     assertNull(appEngineProjectService.getAppEngineStandardDeclaredRuntime(null));
   }
 
-  @Test
   public void testGetAppEngineStandardDeclaredRuntime_NoneDeclared() {
-    ApplicationManager.getApplication()
-        .invokeAndWait(
-            () -> {
-              XmlFile appEngineWebXml = loadTestWebXml("testData/descriptor/appengine-web.xml");
-              assertNull(
-                  appEngineProjectService.getAppEngineStandardDeclaredRuntime(appEngineWebXml));
-            });
+    XmlFile appEngineWebXml = loadTestWebXml("testData/descriptor/appengine-web.xml");
+    assertNull(appEngineProjectService.getAppEngineStandardDeclaredRuntime(appEngineWebXml));
   }
 
-  @Test
   public void testGetAppEngineStandardDeclaredRuntime_Java8() {
-    ApplicationManager.getApplication()
-        .invokeAndWait(
-            () -> {
-              XmlFile appEngineWebXml =
-                  loadTestWebXml("testData/descriptor/appengine-web_runtime-java8.xml");
-              assertEquals(
-                  AppEngineStandardRuntime.JAVA_8,
-                  appEngineProjectService.getAppEngineStandardDeclaredRuntime(appEngineWebXml));
-            });
+    XmlFile appEngineWebXml = loadTestWebXml("testData/descriptor/appengine-web_runtime-java8.xml");
+    assertEquals(AppEngineStandardRuntime.JAVA_8,
+        appEngineProjectService.getAppEngineStandardDeclaredRuntime(appEngineWebXml));
   }
 
-  @Test
   public void testGetAppEngineStandardDeclaredRuntime_Invalid() {
-    ApplicationManager.getApplication()
-        .invokeAndWait(
-            () -> {
-              XmlFile appEngineWebXml =
-                  loadTestWebXml("testData/descriptor/appengine-web_runtime-invalid.xml");
-              assertNull(
-                  appEngineProjectService.getAppEngineStandardDeclaredRuntime(appEngineWebXml));
-            });
+    XmlFile appEngineWebXml = loadTestWebXml(
+        "testData/descriptor/appengine-web_runtime-invalid.xml");
+    assertNull(appEngineProjectService.getAppEngineStandardDeclaredRuntime(appEngineWebXml));
   }
 
   private void addAppEngineStandardFacet(final Module module) {
@@ -161,143 +123,105 @@ public class DefaultAppEngineProjectServiceTest {
 
     return vFile == null
         ? null
-        : (XmlFile) PsiManager.getInstance(testFixture.getProject()).findFile(vFile);
+        : (XmlFile) PsiManager.getInstance(getProject()).findFile(vFile);
   }
 
-  @Test
   public void testGetServiceNameFromAppYaml() throws MalformedYamlFileException {
     assertEquals("javaService",
         appEngineProjectService.getServiceNameFromAppYaml(
             Paths.get(getTestDataPath().toString(), "java.yaml").toString()).get());
   }
 
-  @Test
   public void testGetServiceNameFromAppYaml_noService() throws MalformedYamlFileException {
     assertFalse(appEngineProjectService.getServiceNameFromAppYaml(
         Paths.get(getTestDataPath().toString(), "noservice.yaml").toString()).isPresent());
   }
 
-  @Test
   public void testGetFlexibleRuntimeFromAppYaml_javaRuntime() throws MalformedYamlFileException {
     assertEquals(FlexibleRuntime.JAVA,
         appEngineProjectService.getFlexibleRuntimeFromAppYaml(
             Paths.get(getTestDataPath().toString(), "java.yaml").toString()).get());
   }
 
-  @Test
   public void testGetFlexibleRuntimeFromAppYaml_customRuntime() throws MalformedYamlFileException {
     assertEquals(FlexibleRuntime.CUSTOM,
         appEngineProjectService.getFlexibleRuntimeFromAppYaml(
             Paths.get(getTestDataPath().toString(), "custom.yaml").toString()).get());
   }
 
-  @Test
   public void testGetFlexibleRuntimeFromAppYaml_irregularFormatButValid()
       throws IOException, MalformedYamlFileException {
-    assertEquals(
-        FlexibleRuntime.CUSTOM,
-        appEngineProjectService
-            .getFlexibleRuntimeFromAppYaml(appYamlCustomRuntime.getAbsolutePath())
-            .get());
+    assertEquals(FlexibleRuntime.CUSTOM,
+        appEngineProjectService.getFlexibleRuntimeFromAppYaml(
+            createTempFile("app.yaml", "   runtime :    custom ").getAbsolutePath()).get());
   }
 
-
-  @Test
   public void testGetFlexibleRuntimeFromAppYaml_malformedYaml() throws IOException {
     try {
-      assertEquals(
-          FlexibleRuntime.CUSTOM,
-          appEngineProjectService
-              .getFlexibleRuntimeFromAppYaml(appYamlCustomRuntimeWithEnvVars.getAbsolutePath())
-              .get());
+      assertEquals(FlexibleRuntime.CUSTOM,
+          appEngineProjectService.getFlexibleRuntimeFromAppYaml(
+              createTempFile("app.yaml", "runtime: custom\nenv_variables:\n  'DBG_ENAB")
+                  .getAbsolutePath()).get());
       fail("YAML is malformed.");
     } catch (MalformedYamlFileException myf) {
       // Success.
     }
   }
 
-  @Test
   public void testGetServiceNameFromAppEngineWebXml() {
-    ApplicationManager.getApplication()
-        .invokeAndWait(
-            () -> {
-              assertEquals(
-                  "java8Service",
-                  appEngineProjectService.getServiceNameFromAppEngineWebXml(
-                      loadTestWebXml("testData/descriptor/appengine-web_runtime-java8.xml")));
-            });
+    assertEquals("java8Service",
+        appEngineProjectService.getServiceNameFromAppEngineWebXml(
+            loadTestWebXml("testData/descriptor/appengine-web_runtime-java8.xml")));
   }
 
-  @Test
   public void testGetServiceNameFromAppEngineWebXml_module() {
-    ApplicationManager.getApplication()
-        .invokeAndWait(
-            () -> {
-              assertEquals(
-                  "java8Service",
-                  appEngineProjectService.getServiceNameFromAppEngineWebXml(
-                      loadTestWebXml(
-                          "testData/descriptor/appengine-web_runtime-java8-module.xml")));
-            });
+    assertEquals("java8Service",
+        appEngineProjectService.getServiceNameFromAppEngineWebXml(
+            loadTestWebXml("testData/descriptor/appengine-web_runtime-java8-module.xml")));
   }
 
-  @Test
   public void testGetServiceNameFromAppEngineWebXml_serviceAndModule() {
-    ApplicationManager.getApplication()
-        .invokeAndWait(
-            () -> {
-              assertEquals(
-                  "java8Service",
-                  appEngineProjectService.getServiceNameFromAppEngineWebXml(
-                      loadTestWebXml(
-                          "testData/descriptor/appengine-web_runtime-java8-serviceandmodule.xml")));
-            });
-
+    assertEquals("java8Service",
+        appEngineProjectService.getServiceNameFromAppEngineWebXml(
+            loadTestWebXml(
+                "testData/descriptor/appengine-web_runtime-java8-serviceandmodule.xml")));
   }
 
-  @Test
   public void testGenerateAppYaml() {
-    Path outputDir = Paths.get(testFixture.getProject().getBasePath() + "/src/main/appengine");
-    ApplicationManager.getApplication()
-        .invokeAndWait(
-            () -> {
-              appEngineProjectService.generateAppYaml(
-                  FlexibleRuntime.JAVA, testFixture.getModule(), outputDir);
+    Path outputDir = Paths.get(getProject().getBasePath() + "/src/main/appengine");
+    appEngineProjectService.generateAppYaml(
+        FlexibleRuntime.JAVA,
+        getModule(),
+        outputDir);
 
-              File[] listOfFiles = outputDir.toFile().listFiles();
-              Assert.assertEquals(1, listOfFiles.length);
-              Assert.assertEquals("app.yaml", listOfFiles[0].getName());
-            });
+    File[] listOfFiles = outputDir.toFile().listFiles();
+    Assert.assertEquals(1, listOfFiles.length);
+    Assert.assertEquals("app.yaml", listOfFiles[0].getName());
   }
 
-  @Test
   public void testGenerateDockerfile_war() {
-    Path outputDir = Paths.get(testFixture.getProject().getBasePath() + "/src/main/docker");
-    ApplicationManager.getApplication()
-        .invokeAndWait(
-            () -> {
-              appEngineProjectService.generateDockerfile(
-                  AppEngineFlexibleDeploymentArtifactType.WAR, testFixture.getModule(), outputDir);
+    Path outputDir = Paths.get(getProject().getBasePath() + "/src/main/docker");
+    appEngineProjectService.generateDockerfile(
+        AppEngineFlexibleDeploymentArtifactType.WAR,
+        getModule(),
+        outputDir);
 
-              File[] listOfFiles = outputDir.toFile().listFiles();
-              Assert.assertEquals(1, listOfFiles.length);
-              Assert.assertEquals("Dockerfile", listOfFiles[0].getName());
-            });
+    File[] listOfFiles = outputDir.toFile().listFiles();
+    Assert.assertEquals(1, listOfFiles.length);
+    Assert.assertEquals("Dockerfile", listOfFiles[0].getName());
   }
 
-  @Test
   public void testGenerateDockerfile_jar() {
-    Path outputDir = Paths.get(testFixture.getProject().getBasePath() + "/src/main/docker");
-    ApplicationManager.getApplication()
-        .invokeAndWait(
-            () -> {
-              appEngineProjectService.generateDockerfile(
-                  AppEngineFlexibleDeploymentArtifactType.JAR, testFixture.getModule(), outputDir);
 
-              File[] listOfFiles = outputDir.toFile().listFiles();
-              Assert.assertEquals(1, listOfFiles.length);
-              Assert.assertEquals("Dockerfile", listOfFiles[0].getName());
-            });
+    Path outputDir = Paths.get(getProject().getBasePath() + "/src/main/docker");
+    appEngineProjectService.generateDockerfile(
+        AppEngineFlexibleDeploymentArtifactType.JAR,
+        getModule(),
+        outputDir);
+
+    File[] listOfFiles = outputDir.toFile().listFiles();
+    Assert.assertEquals(1, listOfFiles.length);
+    Assert.assertEquals("Dockerfile", listOfFiles[0].getName());
   }
 
   public static File getTestDataPath() {
