c4b104e8192502a474eaeffc5455c68b8740bdcb
Ivan Portyankin <ivan.portyankin@gmail.com>
2018-02-08 15:58:36 -0500

    remove extra spaces from debug

--- google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/startup/CloudToolsPluginInitializationComponent.java
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/startup/CloudToolsPluginInitializationComponent.java
@@ -49,8 +49,6 @@ public class CloudToolsPluginInitializationComponent implements ApplicationCompo
 
     new ConflictingAppEnginePluginCheck().notifyIfConflicting();
     new GoogleAccountPluginUninstaller().uninstallIfPresent();
-
-
   }
 
   private void initErrorReporting(
