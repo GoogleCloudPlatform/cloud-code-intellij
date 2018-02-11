e3256de0d303f200e09ff3ad658ed54f6c224fe3
Nabila Bashir-Bello <nbashirbello@google.com>
2018-02-11 15:01:24 -0500

    Added call to refresh components

--- google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/cloud/standard/AppEngineStandardStagingPropertiesPanel.java
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/cloud/standard/AppEngineStandardStagingPropertiesPanel.java
@@ -58,6 +58,7 @@ public class AppEngineStandardStagingPropertiesPanel {
           }
         });
 
+    refreshJarSplittingComponents();
     enableJarSplittingCheckBox.addChangeListener(e -> refreshJarSplittingComponents());
   }
 
