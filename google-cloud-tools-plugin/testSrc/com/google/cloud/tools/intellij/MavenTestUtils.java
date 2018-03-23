43d7360310e1ffc43298855d0421710a260e5e11
Etan Shaul <eshaul@google.com>
2018-03-23 12:20:57 -0400

    add test coverage for CloudLibraryDependencyWriter

---
+++ google-cloud-tools-plugin/testSrc/com/google/cloud/tools/intellij/MavenTestUtils.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.intellij;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.module.ModifiableModuleModel;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.module.ModuleWithNameAlreadyExists;
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Computable;
+import java.io.IOException;
+import org.jdom.JDOMException;
+import org.jetbrains.idea.maven.model.MavenId;
+import org.jetbrains.idea.maven.project.MavenProjectsManager;
+import org.jetbrains.idea.maven.wizards.MavenModuleBuilder;
+
+/** Utilities for setting up and working with Maven components in tests. */
+public class MavenTestUtils {
+
+  private static final MavenTestUtils INSTANCE = new MavenTestUtils();
+
+  private MavenTestUtils() {}
+
+  public static MavenTestUtils getInstance() {
+    return INSTANCE;
+  }
+
+  /** Creates a new {@link MavenModuleBuilder} that can be used to create Maven modules in tests. */
+  public MavenModuleBuilder initMavenModuleBuilder(Project project) {
+    MavenModuleBuilder moduleBuilder = new MavenModuleBuilder();
+    MavenId mavenId = new MavenId("org.foo", "module", "1.0");
+
+    String root = project.getBasePath();
+    moduleBuilder.setName("module");
+    moduleBuilder.setModuleFilePath(root + "/module.iml");
+    moduleBuilder.setContentEntryPath(root);
+    moduleBuilder.setProjectId(mavenId);
+
+    return moduleBuilder;
+  }
+
+  /** Creates a new Maven module for use in tests. */
+  public Module createNewMavenModule(MavenModuleBuilder moduleBuilder, Project project) {
+    return ApplicationManager.getApplication()
+        .runWriteAction(
+            (Computable<Module>)
+                () -> {
+                  ModifiableModuleModel model =
+                      ModuleManager.getInstance(project).getModifiableModel();
+                  Module module;
+                  try {
+                    module = moduleBuilder.createModule(model);
+                  } catch (IOException
+                      | ModuleWithNameAlreadyExists
+                      | JDOMException
+                      | ConfigurationException e) {
+                    throw new AssertionError("Error creating Mavenized module");
+                  }
+                  model.commit();
+
+                  resolveDependenciesAndImport(project);
+                  return module;
+                });
+  }
+
+  private void resolveDependenciesAndImport(Project project) {
+    MavenProjectsManager myProjectsManager = MavenProjectsManager.getInstance(project);
+    myProjectsManager.waitForResolvingCompletion();
+    myProjectsManager.performScheduledImportInTests();
+  }
+}
