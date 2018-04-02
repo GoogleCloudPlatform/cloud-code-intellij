29d0c3039336f34517ea39ca849404aff5b00618
Ivan Portyankin <ivan.portyankin@gmail.com>
2018-04-02 12:51:30 -0400

    POC for tracking app engine deployments start/end lifecycle.

--- google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/cloud/executor/AppEngineExecutor.java
+++ google-cloud-tools-plugin/src/com/google/cloud/tools/intellij/appengine/cloud/executor/AppEngineExecutor.java
@@ -16,6 +16,8 @@
 
 package com.google.cloud.tools.intellij.appengine.cloud.executor;
 
+import com.google.cloud.tools.intellij.util.ThreadUtil;
+import com.intellij.execution.process.ProcessWaitFor;
 import com.intellij.openapi.vcs.impl.CancellableRunnable;
 
 /** Executor of {@link AppEngineTask}'s. */
@@ -44,6 +46,14 @@ public class AppEngineExecutor implements CancellableRunnable {
 
   private void setProcess(Process process) {
     this.process = process;
+    System.out.println("app engine executor process started: " + task.toString());
+    ProcessWaitFor processWaitFor =
+        new ProcessWaitFor(
+            this.process,
+            task -> ThreadUtil.getInstance().executeInBackground(task),
+            task.toString());
+    processWaitFor.setTerminationCallback(
+        integer -> System.out.println("app engine executor process exited: " + task.toString()));
   }
 
   public Process getProcess() {
