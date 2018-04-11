1f2a4e5763bb0194cd79af343c5b9a5c9783e447
Nabila Bashir-Bello <nbashirbello@google.com>
2018-04-11 12:30:22 -0400

    Put env var table in a scrollpane to make it look more like the run config table

--- google-cloud-tools-plugin/ultimate/src/com/google/cloud/tools/intellij/apis/ServiceAccountKeyDownloadedPanel.java
+++ google-cloud-tools-plugin/ultimate/src/com/google/cloud/tools/intellij/apis/ServiceAccountKeyDownloadedPanel.java
@@ -69,6 +69,7 @@ public final class ServiceAccountKeyDownloadedPanel {
     tableModel.addRow(new String[] {cloudProjectEnvVar});
     envVarTable.setModel(tableModel);
     envVarTable.setRowSelectionAllowed(false);
+    envVarTable.setTableHeader(null);
 
     // copyToClipboardButton.addActionListener(
     // new CopyToClipboardActionListener(credentialEnvVar + "\n" + cloudProjectEnvVar));
