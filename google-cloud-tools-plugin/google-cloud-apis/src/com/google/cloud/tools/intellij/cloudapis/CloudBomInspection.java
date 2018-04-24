639bef09fd3e48fc836925375e42027df7b98e41
Patrick Flynn <paflynn@google.com>
2018-04-24 15:16:14 -0400

    move all GCP apis management code to own module (#2061)
    * extract out google cloud APIs specific message bundle.
    
    * move all GCP apis management code to own module
    
    Further progress towards #1896
    
    This is the last module to extract of google-cloud-tools-plugin.
    
    Next steps are to minimize deps across the module set and replace static
    inclusions with module dependent inclusions.
    
    * fix formatting

---
+++ google-cloud-tools-plugin/google-cloud-apis/src/com/google/cloud/tools/intellij/cloudapis/CloudBomInspection.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.intellij.cloudapis;
+
+import com.google.cloud.tools.libraries.json.CloudLibrary;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.module.Module;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.util.IncorrectOperationException;
+import com.intellij.util.xml.DomFileElement;
+import com.intellij.util.xml.GenericDomValue;
+import com.intellij.util.xml.highlighting.DomElementAnnotationHolder;
+import com.intellij.util.xml.highlighting.DomElementsInspection;
+import java.util.Set;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.idea.maven.dom.model.MavenDomDependency;
+import org.jetbrains.idea.maven.dom.model.MavenDomProjectModel;
+
+/**
+ * Base {@link DomElementsInspection} for inspecting google-cloud-java BOM related issues in the
+ * project's pom.xml.
+ */
+abstract class CloudBomInspection extends DomElementsInspection<MavenDomProjectModel> {
+
+  private static final Logger logger = Logger.getInstance(CloudBomInspection.class);
+
+  CloudBomInspection() {
+    super(MavenDomProjectModel.class);
+  }
+
+  /**
+   * Iterates through the google-cloud dependencies in the supplied {@link MavenDomProjectModel} and
+   * applies the inspection warning and quickfix if applicable.
+   */
+  @Override
+  public void checkFileElement(
+      DomFileElement<MavenDomProjectModel> domFileElement, DomElementAnnotationHolder holder) {
+    MavenDomProjectModel projectModel = domFileElement.getRootElement();
+    Module module = projectModel.getModule();
+
+    if (module == null) {
+      return;
+    }
+
+    Set<CloudLibrary> cloudLibraries =
+        CloudLibraryProjectState.getInstance(module.getProject()).getCloudLibraries(module);
+
+    if (cloudLibraries.isEmpty()) {
+      return;
+    }
+
+    if (shouldApplyInspection(module)) {
+      projectModel
+          .getDependencies()
+          .getDependencies()
+          .forEach(
+              dependency -> {
+                if (isCloudLibraryDependency(dependency, cloudLibraries)) {
+                  inspectAndFix(dependency, module, holder);
+                }
+              });
+    }
+  }
+
+  /**
+   * Inspections should implement this to indicate if the given BOM inspection is applicable for the
+   * supplied {@link Module}.
+   */
+  abstract boolean shouldApplyInspection(Module module);
+
+  /**
+   * Inspections should implement this to apply the BOM inspection and quickfix.
+   *
+   * @param dependency the dependency on which the inspection and quickfix should be applied
+   * @param module the module of the current pom.xml
+   * @param holder the holder of the inspection warning and quickfix
+   */
+  abstract void inspectAndFix(
+      MavenDomDependency dependency, Module module, DomElementAnnotationHolder holder);
+
+  /** Deletes the supplied version {@link XmlTag}. */
+  void stripVersion(XmlTag versionTag) {
+    try {
+      versionTag.delete();
+    } catch (IncorrectOperationException ioe) {
+      logger.warn("Failed to delete version tag for CloudBomInspection quickfix");
+    }
+  }
+
+  /**
+   * Checks the supplied {@link MavenDomDependency} to see if it is contained in the known set of
+   * managed Google {@link CloudLibrary cloudLibraries}.
+   *
+   * @param dependency the maven dependency we are checking
+   * @param cloudLibraries the set of {@link CloudLibrary cloudLibraries} configured in the project
+   * @return {@code true} if the maven dependency is a google cloud library, and {@code false}
+   *     otherwise
+   */
+  private boolean isCloudLibraryDependency(
+      MavenDomDependency dependency, Set<CloudLibrary> cloudLibraries) {
+    return cloudLibraries
+        .stream()
+        .anyMatch(
+            library ->
+                CloudLibraryUtils.getFirstJavaClientMavenCoordinates(library)
+                    .map(
+                        coords ->
+                            domValueEquals(dependency.getGroupId(), coords.getGroupId())
+                                && domValueEquals(
+                                    dependency.getArtifactId(), coords.getArtifactId()))
+                    .orElse(false));
+  }
+
+  private boolean domValueEquals(GenericDomValue domValue, @Nullable String value) {
+    return domValue != null
+        && domValue.getStringValue() != null
+        && domValue.getStringValue().equals(value);
+  }
+}
---
+++ google-cloud-tools-plugin/google-cloud-apis/src/com/google/cloud/tools/intellij/cloudapis/GoogleCloudApiDetailsPanel.java
@@ -0,0 +1,385 @@
+/*
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.intellij.cloudapis;
+
+import com.google.cloud.tools.intellij.GoogleCloudCoreIcons;
+import com.google.cloud.tools.intellij.cloudapis.CloudApiMavenService.LibraryVersionFromBomException;
+import com.google.cloud.tools.intellij.ui.BrowserOpeningHyperLinkListener;
+import com.google.cloud.tools.intellij.util.ThreadUtil;
+import com.google.cloud.tools.libraries.json.CloudLibrary;
+import com.google.cloud.tools.libraries.json.CloudLibraryClientMavenCoordinates;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
+import com.intellij.icons.AllIcons.General;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
+import com.intellij.ui.IdeBorderFactory;
+import com.intellij.util.SVGLoader;
+import java.awt.Image;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import org.jetbrains.annotations.Nullable;
+
+/** The form-bound class for the Cloud API details panel. */
+public final class GoogleCloudApiDetailsPanel {
+
+  private static final String LINKS_SEPARATOR = " | ";
+
+  private JLabel icon;
+  private JLabel nameLabel;
+  private JLabel versionLabel;
+  private JPanel panel;
+  private JTextPane descriptionTextPane;
+  private JTextPane linksTextPane;
+  private JPanel apiManagementPanel;
+  private JCheckBox enableApiCheckbox;
+  private JPanel managementInfoPanel;
+  private JTextPane managementWarningTextPane;
+
+  private CloudLibrary currentCloudLibrary;
+  private String currentBomVersion;
+  private CloudApiManagementSpec currentCloudApiManagementSpec;
+
+  /** Returns the {@link JPanel} that holds the UI elements in this panel. */
+  JPanel getPanel() {
+    return panel;
+  }
+
+  /**
+   * Sets the currently displayed {@link CloudLibrary} to the given one.
+   *
+   * <p>If the given {@link CloudLibrary} is already displayed, nothing is done and no UI updates
+   * are made.
+   *
+   * @param library the {@link CloudLibrary} to display
+   */
+  void setCloudLibrary(
+      CloudLibrary library, String bomVersion, CloudApiManagementSpec cloudApiManagementSpec) {
+    if (cloudLibrariesEqual(currentCloudLibrary, library)) {
+      return;
+    }
+
+    currentCloudLibrary = library;
+    currentBomVersion = bomVersion;
+    currentCloudApiManagementSpec = cloudApiManagementSpec;
+    updateUI();
+  }
+
+  /**
+   * Enables or disables the components of the GCP API management UI based on if the library is
+   * selected or not.
+   *
+   * @param enabled whether to enable or disable the the management UI components
+   */
+  void setManagementUIEnabled(boolean enabled) {
+    managementInfoPanel.setVisible(!enabled);
+    enableApiCheckbox.setEnabled(enabled);
+
+    // If the checkbox is disabled it should always be unchecked.
+    // Otherwise, it should be checked according to the saved value
+    enableApiCheckbox.setSelected(enabled && currentCloudApiManagementSpec.shouldEnable());
+  }
+
+  /** Returns the {@link JLabel} that holds the library's icon. */
+  @VisibleForTesting
+  JLabel getIcon() {
+    return icon;
+  }
+
+  /** Returns the {@link JLabel} that holds the library's name. */
+  @VisibleForTesting
+  JLabel getNameLabel() {
+    return nameLabel;
+  }
+
+  /** Returns the {@link JLabel} that holds the library's version. */
+  @VisibleForTesting
+  JLabel getVersionLabel() {
+    return versionLabel;
+  }
+
+  /** Returns the {@link JTextPane} that holds the library's description. */
+  @VisibleForTesting
+  JTextPane getDescriptionTextPane() {
+    return descriptionTextPane;
+  }
+
+  /** Returns the {@link JTextPane} that holds the library's links. */
+  @VisibleForTesting
+  JTextPane getLinksTextPane() {
+    return linksTextPane;
+  }
+
+  /** Returns the {@link JCheckBox} that selects if the API should be enabled on GCP. */
+  @VisibleForTesting
+  JCheckBox getEnableApiCheckbox() {
+    return enableApiCheckbox;
+  }
+
+  /**
+   * Returns the {@link JPanel} containing the wording explaining how to enable the management
+   * controls.
+   */
+  @VisibleForTesting
+  JPanel getManagementInfoPanel() {
+    return managementInfoPanel;
+  }
+
+  /**
+   * Initializes some UI components in this panel that require special set-up.
+   *
+   * <p>This is automatically called by the IDEA SDK and should not be directly invoked.
+   */
+  private void createUIComponents() {
+    descriptionTextPane = new JTextPane();
+    descriptionTextPane.setOpaque(false);
+
+    linksTextPane = new JTextPane();
+    linksTextPane.setOpaque(false);
+    linksTextPane.addHyperlinkListener(new BrowserOpeningHyperLinkListener());
+
+    apiManagementPanel = new JPanel();
+    apiManagementPanel.setBorder(
+        IdeBorderFactory.createTitledBorder(
+            GoogleCloudApisMessageBundle.message("cloud.apis.management.section.title")));
+
+    managementWarningTextPane = new JTextPane();
+    managementWarningTextPane.setOpaque(false);
+
+    enableApiCheckbox = new JCheckBox();
+    enableApiCheckbox.addActionListener(
+        event ->
+            currentCloudApiManagementSpec.setShouldEnable(
+                ((JCheckBox) event.getSource()).isSelected()));
+  }
+
+  /**
+   * Updates the UI elements in this panel to reflect the attributes in the {@link
+   * #currentCloudLibrary}.
+   */
+  private void updateUI() {
+    panel.setVisible(true);
+
+    if (currentCloudLibrary.getIcon() == null) {
+      icon.setIcon(null);
+    } else {
+      icon.setIcon(GoogleCloudCoreIcons.LOADING);
+      loadImageAsync(currentCloudLibrary.getIcon(), icon::setIcon);
+    }
+
+    nameLabel.setText(currentCloudLibrary.getName());
+    descriptionTextPane.setText(currentCloudLibrary.getDescription());
+    descriptionTextPane.setSize(
+        descriptionTextPane.getWidth(), descriptionTextPane.getPreferredSize().height);
+
+    if (currentCloudLibrary.getClients() != null) {
+      CloudLibraryUtils.getFirstJavaClient(currentCloudLibrary)
+          .ifPresent(
+              client -> {
+                if (currentBomVersion != null) {
+                  updateManagedLibraryVersionFromBom(currentBomVersion);
+                } else {
+                  if (client.getMavenCoordinates() != null) {
+                    versionLabel.setText(
+                        GoogleCloudApisMessageBundle.message(
+                            "cloud.libraries.version.label",
+                            client.getMavenCoordinates().getVersion()));
+                  }
+                }
+
+                Optional<String> docsLink =
+                    makeLink(
+                        "cloud.libraries.documentation.link",
+                        currentCloudLibrary.getDocumentation());
+                Optional<String> sourceLink =
+                    makeLink("cloud.libraries.source.link", client.getSource());
+                Optional<String> apiReferenceLink =
+                    makeLink("cloud.libraries.apireference.link", client.getApiReference());
+
+                List<Optional<String>> links =
+                    ImmutableList.of(docsLink, sourceLink, apiReferenceLink);
+                linksTextPane.setText(joinLinks(links));
+              });
+    }
+
+    managementWarningTextPane.setText(
+        GoogleCloudApisMessageBundle.message("cloud.apis.management.section.info.text"));
+  }
+
+  /**
+   * Asynchronously fetches and displays the version of the client library that is managed by the
+   * given BOM version.
+   *
+   * @param bomVersion the version of the BOM from which to load the version of the current client
+   *     library
+   */
+  // TODO (eshaul) this unoptimized implementation fetches all managed BOM versions each time the
+  // BOM is updated and library is selected. The bomVersion -> managedLibraryVersions results can be
+  // cached on disk to reduce network calls.
+  @SuppressWarnings("FutureReturnValueIgnored")
+  void updateManagedLibraryVersionFromBom(String bomVersion) {
+    if (currentCloudLibrary.getClients() != null) {
+      CloudLibraryUtils.getFirstJavaClient(currentCloudLibrary)
+          .ifPresent(
+              client -> {
+                CloudLibraryClientMavenCoordinates coordinates = client.getMavenCoordinates();
+
+                if (coordinates != null) {
+                  versionLabel.setIcon(GoogleCloudCoreIcons.LOADING);
+                  versionLabel.setText("");
+
+                  ThreadUtil.getInstance()
+                      .executeInBackground(
+                          () -> {
+                            try {
+                              Optional<String> versionOptional =
+                                  CloudApiMavenService.getInstance()
+                                      .getManagedDependencyVersion(coordinates, bomVersion);
+
+                              if (versionOptional.isPresent()) {
+                                ApplicationManager.getApplication()
+                                    .invokeAndWait(
+                                        () -> {
+                                          versionLabel.setText(
+                                              GoogleCloudApisMessageBundle.message(
+                                                  "cloud.libraries.version.label",
+                                                  versionOptional.get()));
+                                        },
+                                        ModalityState.any());
+
+                                versionLabel.setIcon(null);
+                              } else {
+                                versionLabel.setText(
+                                    GoogleCloudApisMessageBundle.message(
+                                        "cloud.libraries.version.label",
+                                        GoogleCloudApisMessageBundle.message(
+                                            "cloud.libraries.version.notfound.text", bomVersion)));
+                                versionLabel.setIcon(General.Error);
+                              }
+                            } catch (LibraryVersionFromBomException ex) {
+                              versionLabel.setText(
+                                  GoogleCloudApisMessageBundle.message(
+                                      "cloud.libraries.version.label",
+                                      GoogleCloudApisMessageBundle.message(
+                                          "cloud.libraries.version.exception.text")));
+                              versionLabel.setIcon(General.Error);
+                            }
+                          });
+                }
+              });
+    }
+  }
+
+  /**
+   * Loads the image at the given URL asynchronously and passes the resulting {@link ImageIcon} to
+   * the given {@link Consumer} when finished.
+   *
+   * <p>Note that while the loading of the image is done asynchronously, the call to {@link
+   * Consumer#accept(Object)} is done on the EDT.
+   *
+   * @param imageUrl the URL to load the image from. If this is {@code null}, the given consumer is
+   *     immediately called to accept {@code null}.
+   * @param imageConsumer the {@link Consumer} for the final resulting {@link ImageIcon}. If there
+   *     was a problem loading the image, {@code null} will be passed to this consumer.
+   */
+  @SuppressWarnings("FutureReturnValueIgnored")
+  private static void loadImageAsync(@Nullable String imageUrl, Consumer<ImageIcon> imageConsumer) {
+    if (imageUrl == null) {
+      imageConsumer.accept(null);
+      return;
+    }
+
+    ThreadUtil.getInstance()
+        .executeInBackground(
+            () -> {
+              ImageIcon imageIcon = null;
+              try {
+                URL iconUrl = new URL(imageUrl);
+                Image image = SVGLoader.load(iconUrl, 0.5f);
+                if (image != null) {
+                  imageIcon = new ImageIcon(image);
+                }
+              } catch (IOException e) {
+                // Do nothing.
+              }
+
+              ImageIcon finalImageIcon = imageIcon;
+              ApplicationManager.getApplication()
+                  .invokeAndWait(() -> imageConsumer.accept(finalImageIcon), ModalityState.any());
+            });
+  }
+
+  /**
+   * Optionally returns an HTML-formatted link for the given URL.
+   *
+   * @param key the key of the text (via {@link
+   *     com.google.cloud.tools.intellij.cloudapis.GoogleCloudApisMessageBundle#message}) to show
+   *     for the link
+   * @param url the URL to make into an HTML link
+   * @return the HTML-formatted link, or {@link Optional#empty()} if the given URL is {@code null}
+   */
+  private static Optional<String> makeLink(String key, @Nullable String url) {
+    if (url == null) {
+      return Optional.empty();
+    }
+    return Optional.of(
+        String.format("<a href=\"%s\">%s</a>", url, GoogleCloudApisMessageBundle.message(key)));
+  }
+
+  /**
+   * Joins the given list of HTML links into a delimited string, separated by {@link
+   * #LINKS_SEPARATOR} and wrapped by {@literal <html>} tags.
+   *
+   * @param links the list of {@link Optional} links to join. Only links that are present are joined
+   *     together; {@link Optional#empty()} values are ignored.
+   */
+  private static String joinLinks(List<Optional<String>> links) {
+    String joinedLinks =
+        links
+            .stream()
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .collect(Collectors.joining(LINKS_SEPARATOR));
+    return String.format(
+        "<html><body><p style=\"margin-top: 0\">%s</p></body></html>", joinedLinks);
+  }
+
+  /**
+   * Returns {@code true} if the given {@link CloudLibrary CloudLibraries} are equal, {@code false}
+   * otherwise.
+   */
+  // TODO(nkibler): Add an equals() implementation to the CloudLibrary class that does a deep field
+  // comparison.
+  private static boolean cloudLibrariesEqual(CloudLibrary library1, CloudLibrary library2) {
+    if (library1 == library2) {
+      return true;
+    } else if (library1 == null || library2 == null) {
+      return false;
+    }
+    return Objects.equals(library1.getId(), library2.getId());
+  }
+}
