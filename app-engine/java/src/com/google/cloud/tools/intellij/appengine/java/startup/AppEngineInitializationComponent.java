fd06344871d0b55cfe141869ed63fc3872b18629
Patrick Flynn <paflynn@google.com>
2018-05-10 15:08:12 -0400

    added class comment to initialization component.

--- app-engine/java/src/com/google/cloud/tools/intellij/appengine/java/startup/AppEngineInitializationComponent.java
+++ app-engine/java/src/com/google/cloud/tools/intellij/appengine/java/startup/AppEngineInitializationComponent.java
@@ -21,6 +21,9 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.components.ServiceManager;
 
+/**
+ * Run at plugin startup to do App Engine specific plugin initialization.
+ */
 public class AppEngineInitializationComponent implements ApplicationComponent {
 
   @Override
