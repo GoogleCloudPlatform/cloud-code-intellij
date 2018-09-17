d1e209986c443e3d22201b64bebead9d2bf80be7
Ivan Porty <ivan.portyankin@gmail.com>
2018-09-17 16:48:19 -0400

    Adds initial IDE plugin manifest XML files for core and Skaffold (#11)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
-repositories {
-    jcenter()
-}
-
 plugins {
     id("org.jetbrains.intellij") version "0.3.7"
 
     kotlin("jvm") version "1.2.61"
+}
+
+allprojects {
+    repositories {
+        jcenter()
+    }
+
+    apply(plugin = "org.jetbrains.intellij")
+    apply(plugin = "kotlin")
 }
