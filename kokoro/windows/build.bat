c3e72846701cfa887cd243c935997441bdd59a06
Etan Shaul <eshaul@google.com>
2018-09-26 15:12:33 -0400

    Fix incorrect path in windows kokoro script (#31)

--- kokoro/windows/build.bat
+++ kokoro/windows/build.bat
@@ -27,7 +27,7 @@
 @rem (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 @rem OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-cd github/google-cloud-intellij
+cd github/google-container-tools-intellij
 
 @rem Explicitly set to JDK 8 to get around Kokoro default environment value of JDK 9
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_144
