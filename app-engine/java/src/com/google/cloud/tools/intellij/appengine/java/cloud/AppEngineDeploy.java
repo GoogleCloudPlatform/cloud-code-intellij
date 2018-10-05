aa9b285a806920d84e53e99752b7e3172e3fd061
Etan Shaul <eshaul@google.com>
2018-10-05 10:57:12 -0400

    Stop the deployment operation when Cloud SDK is out of date (#2272)

--- app-engine/java/src/com/google/cloud/tools/intellij/appengine/java/cloud/AppEngineDeploy.java
+++ app-engine/java/src/com/google/cloud/tools/intellij/appengine/java/cloud/AppEngineDeploy.java
@@ -154,7 +154,11 @@ public class AppEngineDeploy {
     try {
       helper.createGcloud(loggingHandler).newDeployment(processHandler).deploy(configuration);
     } catch (CloudSdkOutOfDateException ex) {
-      logger.warn("Error during deployment because because Cloud SDK version out of date");
+      logger.warn("Deployment was terminated because the Cloud SDK version is out of date", ex);
+      callback.errorOccurred(
+          AppEngineMessageBundle.message("appengine.deployment.error.sdk.out.of.date")
+              + "\n"
+              + AppEngineMessageBundle.message("appengine.action.error.update.message"));
     }
   }
 
