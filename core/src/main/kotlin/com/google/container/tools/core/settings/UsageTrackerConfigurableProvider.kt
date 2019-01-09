38a555e22281652014c3a24e74a7ad14e9299cdd
Etan Shaul <eshaul@google.com>
2019-01-09 14:19:49 -0500

    Fix for shared panel bug for users of old GCT plugin (#121)

--- core/src/main/kotlin/com/google/container/tools/core/settings/UsageTrackerConfigurableProvider.kt
+++ core/src/main/kotlin/com/google/container/tools/core/settings/UsageTrackerConfigurableProvider.kt
@@ -20,6 +20,8 @@ import com.google.container.tools.core.analytics.UsageTrackerManagerService
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.options.ConfigurableProvider
 
+private const val USAGE_TRACKER_PROVIDER_NAME = "UsageTrackerConfigurableProvider"
+
 /**
  * Class that provides the configurable which creates the "Usage Tracking" menu item under the
  * top-level "Google" menu item.
@@ -32,6 +34,21 @@ class UsageTrackerConfigurableProvider : ConfigurableProvider() {
      * Only create the menu item if usage tracking is available. For example, if running in dev
      * mode with no analytics ID environment variable configured, hide the usage track menu item.
      */
-    override fun canCreateConfigurable(): Boolean =
-        UsageTrackerManagerService.instance.isUsageTrackingAvailable()
+    override fun canCreateConfigurable(): Boolean {
+        /**
+         * This check is a workaround for the fact that the older versions of the GCT plugin do not
+         * have an ID configured for this panel. Since we only want a single instance of this panel
+         * to appear for all installed Google plugins, this checks to ensure that it was not already
+         * registered by the GCT plugin. Once all users migrate to newer versions of the GCT plugin,
+         * we can remove this. For this to work, both provider classes have to have the same name.
+         */
+        val canCreateConfigurable: Boolean =
+            Configurable.APPLICATION_CONFIGURABLE.extensionList.filter {
+                it?.providerClass != null &&
+                    it.providerClass.endsWith(USAGE_TRACKER_PROVIDER_NAME)
+            }.size == 1
+
+        return canCreateConfigurable &&
+            UsageTrackerManagerService.instance.isUsageTrackingAvailable()
+    }
 }
