7ade357ee08c3bc51c4bc025fc570a734d0f4d1f
Ivan Porty <ivan.portyankin@gmail.com>
2019-01-10 10:40:21 -0500

    Send metrics on Skaffold add configurations using project events and message bus. (#122)

--- skaffold/src/main/kotlin/com/google/container/tools/skaffold/metrics/SkaffoldTracking.kt
+++ skaffold/src/main/kotlin/com/google/container/tools/skaffold/metrics/SkaffoldTracking.kt
@@ -22,6 +22,9 @@ const val SKAFFOLD_DEV_RUN_FAIL = "skaffold.dev.run.fail"
 const val SKAFFOLD_SINGLE_RUN_SUCCESS = "skaffold.single.run"
 const val SKAFFOLD_SINGLE_RUN_FAIL = "skaffold.single.run.fail"
 
+const val SKAFFOLD_DEV_CONFIGURATION_ADDED = "skaffold.dev.config.added"
+const val SKAFFOLD_SINGLE_RUN_CONFIGURATION_ADDED = "skaffold.single.run.config.added"
+
 const val SKAFFOLD_AUTO_CREATE_CONFIGURATIONS = "skaffold.auto.create.configs"
 
 /** Generic metadata field name for any error message (or error name) information. */
